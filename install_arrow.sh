269ecb8b9737cfdd93415020292b3fd7332f18fc
yannis <yannis@node-0.yannis-qv63223.hustle-pg0.wisc.cloudlab.us>
2020-01-13 05:02:26 -0600

    fix scripts

--- install_arrow.sh
+++ install_arrow.sh
@@ -1,6 +1,6 @@
 sudo apt update
-sudo apt install -y -V apt-transport-https curl gnupg lsb-release
-sudo curl --output /usr/share/keyrings/apache-arrow-keyring.gpg https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/apache-arrow-keyring.gpg
+sudo apt install -y -V apt-transport-https gnupg lsb-release wget
+sudo wget -O /usr/share/keyrings/apache-arrow-keyring.gpg https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/apache-arrow-keyring.gpg
 sudo tee /etc/apt/sources.list.d/apache-arrow.list <<APT_LINE
 deb [arch=amd64 signed-by=/usr/share/keyrings/apache-arrow-keyring.gpg] https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/ $(lsb_release --codename --short) main
 deb-src [signed-by=/usr/share/keyrings/apache-arrow-keyring.gpg] https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/ $(lsb_release --codename --short) main
