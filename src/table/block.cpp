e1427e997367f2c78f4d8a79387677c02efa6236
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-01-27 14:22:54 -0600

    vector of ArrayData instead of RecordBatch. Read block from file needs to be fixed.

--- src/table/block.cpp
+++ src/table/block.cpp
@@ -16,9 +16,6 @@ Block::Block(int id, const std::shared_ptr<arrow::Schema> &in_schema,
                                  &schema);
     evaluate_status(status, __FUNCTION__, __LINE__);
 
-    std::vector<std::shared_ptr<arrow::ArrayData>> columns;
-
-
     for (const auto &field : schema->fields()) {
 
         // Empty ArrayData should be constructed using the constructor that
@@ -71,32 +68,27 @@ Block::Block(int id, const std::shared_ptr<arrow::Schema> &in_schema,
         }
     }
 
-    records = arrow::RecordBatch::Make(this->schema, 0, columns);
 }
 
 
 int Block::compute_num_bytes() {
 
     // Start at i=1 to skip valid column
-    for (int i = 1; i < records->num_columns(); i++) {
+    for (int i = 1; i < schema->num_fields(); i++) {
 
-        std::shared_ptr<arrow::Field> field = records->schema()->field(i);
-
+        std::shared_ptr<arrow::Field> field = schema->field(i);
         switch (field->type()->id()) {
 
             case arrow::Type::STRING: {
-                auto column = std::static_pointer_cast<arrow::StringArray>(
-                        records->column(i));
-                num_bytes += column->value_offset(column->length());
+                //TODO(nicholas): is this correct??
+                num_bytes += columns[i]->length;
                 break;
             }
             case arrow::Type::BOOL:
             case arrow::Type::INT64: {
-                auto column = std::static_pointer_cast<arrow::FixedSizeBinaryArray>(
-                        records->column(i));
                 // buffer at index 1 is the data buffer.
                 int byte_width = field->type()->layout().bit_widths[1] / 8;
-                num_bytes += byte_width * column->length();
+                num_bytes += byte_width * columns[i]->length;
                 break;
             }
             default: {
@@ -114,47 +106,50 @@ Block::Block(int id, std::shared_ptr<arrow::RecordBatch> record_batch,
         : capacity(capacity), id(id), num_bytes(0) {
 
     num_rows = record_batch->num_rows();
-    records = std::move(record_batch);
+    //TODO(nicholas): fix initialization from RecordBatch
+//    records = std::move(record_batch);
     compute_num_bytes();
 }
 
-std::shared_ptr<arrow::RecordBatch> Block::get_records() { return records; }
+std::shared_ptr<arrow::RecordBatch> Block::get_records() { return
+    arrow::RecordBatch::Make(schema, num_rows, columns); }
 
 int Block::get_num_rows() const { return num_rows; }
 
 int Block::get_id() const { return id; }
 
 std::shared_ptr<arrow::Array> Block::get_column(int column_index) const {
-    return records->column(column_index);
+    return arrow::MakeArray(columns[column_index]);
 }
 
 std::shared_ptr<arrow::Array>
 Block::get_column_by_name(const std::string &name) const {
-    return records->GetColumnByName(name);
+    // TODO(nicholas): need ot search for column by name
+    return nullptr;
 }
 
 int Block::get_free_row_index() const {
-    auto valid = std::static_pointer_cast<arrow::BooleanArray>(
-            records->column(0));
-    for (int row_index = 0; row_index < num_rows + 1; ++row_index) {
-        if (!valid->Value(row_index)) {
-            return row_index;
-        }
-    }
+    //TODO(nicholas)
+//    auto valid = std::static_pointer_cast<arrow::BooleanArray>(
+//            columns[0]);
+//    for (int row_index = 0; row_index < num_rows + 1; ++row_index) {
+//        if (!valid->Value(row_index)) {
+//            return row_index;
+//        }
+//    }
 
     return -1;
 }
 
 bool Block::get_valid(unsigned int row_index) const {
-    auto valid = std::static_pointer_cast<arrow::BooleanArray>(
-            records->column(0));
-    return valid->Value(row_index);
+    auto *data = columns[0]->GetMutableValues<uint8_t>(1, 0);
+    uint8_t byte = data[row_index / 8];
+    return (byte >> (row_index % 8u)) == 1;
+
 }
 
 void Block::set_valid(unsigned int row_index, bool val) {
-    auto valid = std::static_pointer_cast<arrow::BooleanArray>(
-            records->column(0));
-    auto *data = valid->values()->mutable_data();
+    auto *data = columns[0]->GetMutableValues<uint8_t>(1, 0);
     if (val) {
         data[row_index / 8] |= (1u << (row_index % 8u));
     } else {
@@ -191,15 +186,21 @@ int Block::get_bytes_left() {
 
 void Block::print() {
 
-    for (int row = 0; row < num_rows; row++) {
-        for (int i = 0; i < records->schema()->num_fields(); i++) {
+    // Create Arrays from ArrayData so we can easily read column data
+    std::vector<std::shared_ptr<arrow::Array>> arrays;
+    for (int i = 0; i < schema->num_fields(); i++) {
+        arrays.push_back(arrow::MakeArray(columns[i]));
+    }
 
-            int type = records->schema()->field(i)->type()->id();
+    for (int row = 0; row < num_rows; row++) {
+        for (int i = 0; i < schema->num_fields(); i++) {
+
+            int type = schema->field(i)->type()->id();
 
             switch (type) {
                 case arrow::Type::STRING: {
                     auto col = std::static_pointer_cast<arrow::StringArray>(
-                            records->column(i));
+                            arrays[i]);
                     auto test = col->value_data()->data();
 
                     std::cout << col->GetString(row) << "\t";
@@ -207,19 +208,19 @@ void Block::print() {
                 }
                 case arrow::Type::type::FIXED_SIZE_BINARY: {
                     auto col = std::static_pointer_cast<arrow::FixedSizeBinaryArray>(
-                            records->column(i));
+                            arrays[i]);
                     std::cout << col->GetString(row) << "\t";
                     break;
                 }
                 case arrow::Type::type::INT64: {
                     auto col = std::static_pointer_cast<arrow::Int64Array>(
-                            records->column(i));
+                            arrays[i]);
                     std::cout << col->Value(row) << "\t";
                     break;
                 }
                 case arrow::Type::BOOL: {
                     auto col = std::static_pointer_cast<arrow::BooleanArray>(
-                            records->column(i));
+                            arrays[i]);
                     std::cout << col->Value(row) << "\t";
                     break;
                 }
@@ -227,7 +228,7 @@ void Block::print() {
                     throw std::logic_error(
                             std::string(
                                     "Block created with unsupported type: ") +
-                            records->schema()->field(i)->type()->ToString());
+                            schema->field(i)->type()->ToString());
                 }
             }
         }
@@ -245,18 +246,16 @@ bool Block::insert_record(uint8_t *record, int32_t *byte_widths) {
     // Position in the record array
     int head = 0;
 
-    for (int i = 0; i < records->num_columns(); i++) {
+    for (int i = 0; i < schema->num_fields(); i++) {
 
-        std::shared_ptr<arrow::Field> field = records->schema()->field(i);
+        std::shared_ptr<arrow::Field> field = schema->field(i);
         switch (field->type()->id()) {
             // Although BOOL is a fixed-width type, we must handle it
             // separately, since data is stored at the bit-level rather than
             // the byte level.
             case arrow::Type::BOOL: {
-                auto column = std::static_pointer_cast<arrow::BooleanArray>(
-                        records->column(i));
                 auto data_buffer = std::static_pointer_cast<arrow::ResizableBuffer>(
-                        records->column(i)->data()->buffers[1]);
+                        columns[i]->buffers[1]);
 
                 // Extended the underlying buffers. This may result in copying
                 // the data.
@@ -266,17 +265,15 @@ bool Block::insert_record(uint8_t *record, int32_t *byte_widths) {
                 }
 
                 set_valid(num_rows, true);
-                column->data()->length++;
+                columns[i]->length++;
                 break;
             }
 
             case arrow::Type::STRING: {
-                auto column = std::static_pointer_cast<arrow::StringArray>(
-                        records->column(i));
                 auto offsets_buffer = std::static_pointer_cast<arrow::ResizableBuffer>(
-                        column->data()->buffers[1]);
+                        columns[i]->buffers[1]);
                 auto data_buffer = std::static_pointer_cast<arrow::ResizableBuffer>(
-                        column->data()->buffers[2]);
+                        columns[i]->buffers[2]);
 
                 // Extended the underlying data and offsets buffer. This may
                 // result in copying the data.
@@ -290,7 +287,7 @@ bool Block::insert_record(uint8_t *record, int32_t *byte_widths) {
                 evaluate_status(status, __FUNCTION__, __LINE__);
 
                 // Insert new offset
-                auto *offsets_data = column->data()->GetMutableValues<int32_t>(
+                auto *offsets_data = columns[i]->GetMutableValues<int32_t>(
                         1, 0);
                 int32_t new_offset =
                         offsets_data[num_rows] + byte_widths[i - 1];
@@ -298,35 +295,30 @@ bool Block::insert_record(uint8_t *record, int32_t *byte_widths) {
                             sizeof(new_offset));
 
                 // Insert new data
-                int32_t x = column->value_offset(num_rows);
-                auto *values_data = column->data()->GetMutableValues<uint8_t>(
-                        2, column->value_offset(num_rows));
+                int32_t x = offsets_data[num_rows]; //TODO(nicholas): correct?
+                auto *values_data = columns[i]->GetMutableValues<uint8_t>(
+                        2, offsets_data[num_rows]);
                 std::memcpy(values_data, &record[head], byte_widths[i - 1]);
 
-                column->data()->length++;
+                columns[i]->length++;
                 head += byte_widths[i - 1];
                 break;
             }
                 // This works with any fixed-width type, but for now, I specify INT64
             case arrow::Type::INT64: {
 
-                std::shared_ptr<arrow::Array> column = records->column(i);
-
                 auto data_buffer = std::static_pointer_cast<arrow::ResizableBuffer>(
-                        column->data()->buffers[1]);
+                        columns[i]->buffers[1]);
                 status = data_buffer->Resize(
                         data_buffer->size() + byte_widths[i - 1]);
                 evaluate_status(status, __FUNCTION__, __LINE__);
 
-                column = std::static_pointer_cast<arrow::FixedSizeBinaryArray>(
-                        records->column(i));
-
-                auto *dest = column->data()->GetMutableValues<uint8_t>(
+                auto *dest = columns[i]->GetMutableValues<uint8_t>(
                         1, num_rows * byte_widths[i - 1]);
                 std::memcpy(dest, &record[head], byte_widths[i - 1]);
 
                 head += byte_widths[i - 1];
-                column->data()->length++;
+                columns[i]->length++;
                 break;
             }
 
@@ -339,12 +331,12 @@ bool Block::insert_record(uint8_t *record, int32_t *byte_widths) {
     }
     increment_num_bytes(head);
     increment_num_rows();
-
+    print();
     // Create a new RecordBatch object to assure that records->num_rows_ is
     // consistent with the length of the underlying ArrayData.
-    records = arrow::RecordBatch::Make(records->schema(),
-                                       num_rows,
-                                       get_columns_from_record_batch(records));
+//    records = arrow::RecordBatch::Make(records->schema(),
+//                                       num_rows,
+//                                       get_columns_from_record_batch(records));
 }
 
 
