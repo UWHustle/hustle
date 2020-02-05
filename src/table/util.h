b25d7d0ac8027ffba260ae3841bdc80bfae32e8f
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-02-05 16:45:43 -0600

    Can now specify block size for csv reader

--- src/table/util.h
+++ src/table/util.h
@@ -73,6 +73,6 @@ int compute_fixed_record_width(std::shared_ptr<arrow::Schema> schema);
 
 
 Table read_from_csv_file(const char* path, std::shared_ptr<arrow::Schema>
-        schema);
+schema, int block_size);
 
 #endif //HUSTLE_OFFLINE_UTIL_H
