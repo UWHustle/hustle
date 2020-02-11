4f6a0d5b5cf22cc7d147e77ca096cb70aef94ecb
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-02-11 11:24:05 -0600

    Select now operates on a single block

--- src/operators/Operator.h
+++ src/operators/Operator.h
@@ -9,7 +9,11 @@ namespace operators {
 
 class Operator{
  public:
-  virtual std::vector<std::shared_ptr<Block>> runOperator(std::vector<std::vector<std::shared_ptr<Block>>> block_groups) = 0;
+
+    // TODO(nicholas): I don't like the whole vector of Tables stuff going on
+    //  here. Try to find a better call signature.
+  virtual std::vector<std::shared_ptr<Block>> runOperator
+  (std::vector<std::vector<std::shared_ptr<Block>>> block_groups) = 0;
 
 };
 
