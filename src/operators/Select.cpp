97c438b0684bf00a506efa96891c92b281296ce7
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-02-11 11:24:05 -0600

    Select now operates on a single block

--- src/operators/Select.cpp
+++ src/operators/Select.cpp
@@ -17,6 +17,46 @@ Select::Select(arrow::compute::CompareOperator compare_operator, std::string col
   column_value_ = std::move(column_value);
 }
 
+std::shared_ptr<Block> Select::runOperatorOnBlock(std::shared_ptr<Block>
+        block) {
+
+    arrow::Status status;
+
+    // TODO(nicholas): Should the function context be created in the inner or
+    //  outer loop? I think we only need it in the inner loop if selects are
+    //  being executed in parallel
+    std::vector<std::shared_ptr<arrow::ArrayData>> out_data;
+    arrow::compute::FunctionContext function_context(arrow::default_memory_pool());
+    arrow::compute::CompareOptions compare_options(compare_operator_);
+    auto* filter = new arrow::compute::Datum();
+
+    status = arrow::compute::Compare(
+            &function_context,
+            block->get_column_by_name(column_name_),
+            column_value_,
+            compare_options,
+            filter
+    );
+    evaluate_status(status, __FUNCTION__, __LINE__);
+
+    auto* out_col = new arrow::compute::Datum();
+    for (int j = 0; j < block->get_records()->num_columns(); j++) {
+        status = arrow::compute::Filter(&function_context, block->get_column(j),
+                                        *filter, out_col);
+
+        evaluate_status(status, __FUNCTION__, __LINE__);
+        out_data.push_back(out_col->array());
+    }
+
+    // TODO(nicholas): Block constructor should accept vector of ArrayData,
+    //  not vector of RecordBatches
+    auto out_batch = arrow::RecordBatch::Make(std::make_shared<arrow::Schema>
+            (arrow::Schema(*block->get_records()->schema())),
+            out_data[0]->length, out_data);
+
+    return  std::make_shared<Block>(Block(rand(), out_batch, BLOCK_SIZE));
+}
+
 std::vector<std::shared_ptr<Block>> Select::runOperator(std::vector<std::vector<std::shared_ptr<Block>>> block_groups) {
   // operator only uses first block group, ignore others
   auto blocks = block_groups[0];
@@ -24,39 +64,7 @@ std::vector<std::shared_ptr<Block>> Select::runOperator(std::vector<std::vector<
   arrow::Status status;
   auto out = std::vector<std::shared_ptr<Block>>();
   for (int i = 0; i < blocks.size(); i++) {
-    // select on each block separately
-    // setup
-    std::vector<std::shared_ptr<arrow::ArrayData>> out_data;
-    arrow::compute::FunctionContext function_context(arrow::default_memory_pool());
-    arrow::compute::CompareOptions compare_options(compare_operator_);
-    auto* filter = new arrow::compute::Datum();
-    // computation
-    status = arrow::compute::Compare(
-        &function_context,
-        blocks[i]->get_records()->GetColumnByName(column_name_),
-        column_value_,
-        compare_options,
-        filter
-        );
-    if (!status.ok()) {
-      // compare failed, could not generate filter
-      evaluate_status(status, __FUNCTION__, __LINE__);
-    }
-    auto* out_col = new arrow::compute::Datum();
-    for (int j = 0; j < blocks[i]->get_records()->schema()->num_fields(); j++) {
-      status = arrow::compute::Filter(&function_context, blocks[i]->get_records()->column(j), *filter, out_col);
-      if(!status.ok()) {
-        // filter failed
-        evaluate_status(status, __FUNCTION__, __LINE__);
-      }
-      out_data.push_back(out_col->array());
-    }
-    // create RecordBatch and Block from result
-    // copy schema
-    auto out_batch = arrow::RecordBatch::Make(std::make_shared<arrow::Schema>(arrow::Schema(*blocks[i]->get_records()->schema())), out_data[0]->length, out_data);
-    std::shared_ptr<Block> out_block = std::make_shared<Block>(Block(rand(), out_batch, BLOCK_SIZE));
-    out.push_back(out_block);
-    out_data.clear();
+    out.push_back(runOperatorOnBlock(blocks[i]));
   }
   return out;
 }
