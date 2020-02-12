84b825cfc62adc4647c9bbe08ea540832c6ae9e7
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-02-11 18:55:46 -0600

    Started cleaning up code; not finished

--- src/operators/Join.cpp
+++ src/operators/Join.cpp
@@ -5,6 +5,7 @@
 #include <arrow/compute/kernels/compare.h>
 #include <table/util.h>
 #include <iostream>
+#include <arrow/scalar.h>
 
 #define BLOCK_SIZE 1024
 
@@ -13,6 +14,86 @@ namespace operators {
 
 Join::Join(std::string column_name) {
   column_name_ = std::move(column_name);
+}
+
+std::shared_ptr<Block> Join::runOperator(
+        std::shared_ptr<Block> left, std::shared_ptr<Block> right) {
+
+    arrow::Status status;
+    std::vector<std::shared_ptr<arrow::ArrayData>> out_record_data;
+
+    auto schema_fields = std::vector<std::shared_ptr<arrow::Field>>();
+    for (int j = 0; j < left->get_records()->schema()->num_fields(); j++) {
+        schema_fields.push_back(left->get_records()->schema()->field(j)->Copy());
+    }
+    for (int j = 0; j < right->get_records()->schema()->num_fields(); j++) {
+        if (right->get_records()->schema()->field(j)->name() != column_name_) {
+            schema_fields.push_back(right->get_records()->schema()->field(j)->Copy());
+        }
+    }
+    auto out_schema = arrow::schema(schema_fields);
+
+
+
+    for (int i=0; i< left->get_num_rows(); i++) {
+
+        // Look at the value in the Left Table we want to join with the
+        // entire right table
+        arrow::compute::Datum join_val;
+        auto join_val_field = left->get_records()->schema()->GetFieldByName(column_name_);
+        switch (join_val_field->type()->id()) {
+            case arrow::Type::INT64: {
+                auto casted_col = std::static_pointer_cast<arrow::Int64Array>(
+                        left->get_records()->GetColumnByName(column_name_)
+                );
+                join_val = arrow::compute::Datum(casted_col->raw_values()[i]);
+            }
+                break;
+            default: {
+                throw std::logic_error(
+                        std::string("Unsupported type for join operation: ") +
+                        join_val_field->type()->ToString());
+            }
+        }
+
+
+
+        arrow::compute::FunctionContext function_context(arrow::default_memory_pool());
+        arrow::compute::CompareOptions compare_options(arrow::compute::CompareOperator::EQUAL);
+        auto *filter = new arrow::compute::Datum();
+        // computation
+        status = arrow::compute::Compare(
+                &function_context,
+                join_val,
+                right->get_records()->GetColumnByName(column_name_),
+                compare_options,
+                filter
+        );
+
+        // TODO(nicholas): This is not a natural join; I do not skip the
+        //  duplicate join column.
+        for (int col_index=0; col_index<right->get_num_rows(); col_index++) {
+            auto *out_data = new arrow::compute::Datum();
+            status = arrow::compute::Filter(
+                    &function_context,
+                    right->get_records()->column(col_index),
+                    *filter,
+                    out_data);
+            out_record_data.push_back(out_data->array());
+        }
+
+        for (int col_index=0; col_index<left->get_num_rows(); col_index++) {
+            auto scalar = std::make_shared<arrow::Int64Scalar>(join_val);
+            std::shared_ptr<arrow::Array> left_col;
+            arrow::MakeArrayFromScalar(*scalar,
+                                       out_record_data[0]->length,
+                                       &left_col);
+
+            out_record_data.push_back(left_col->data());
+        }
+
+    }
+
 }
 
 std::vector<std::shared_ptr<Block>> Join::runOperator(std::vector<std::vector<std::shared_ptr<Block>>> block_groups) {
