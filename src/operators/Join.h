b390ab8bc818c9b15134f9c0a4600a81483eee9e
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-02-21 18:36:00 -0600

    Hash join added. Nested loops join commented out.

--- src/operators/Join.h
+++ src/operators/Join.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <table/block.h>
+#include <table/table.h>
 #include <arrow/compute/api.h>
 
 #include "Operator.h"
@@ -10,14 +11,23 @@
 namespace hustle {
 namespace operators {
 
+struct record_id {
+    int block_id;
+    int row_index;
+};
+
 class Join : public Operator{
  public:
   explicit Join(std::string column_name);
 
   // Operator.h
   std::vector<std::shared_ptr<Block>> runOperator(std::vector<std::vector<std::shared_ptr<Block>>> block_groups) override;
-    std::shared_ptr<Block> runOperator(
-            std::shared_ptr<Block> left, std::shared_ptr<Block> right);
+  std::vector<std::shared_ptr<Block>> runOperator(
+          std::shared_ptr<arrow::Schema> out_schema,
+            arrow::compute::Datum left_join_val, std::shared_ptr<Table> right);
+
+  std::shared_ptr<Table> hash_join(std::shared_ptr<Table> left_table, std::shared_ptr<Table>
+          right_table);
 
  private:
   std::string column_name_;
