a8b595bf0765eae036dd1488873837fe4ff82703
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-02-28 12:50:24 -0600

    Reorganization

--- src/operators/Select.cpp
+++ src/operators/Select.cpp
@@ -11,40 +11,124 @@
 namespace hustle {
 namespace operators {
 
-Select::Select(arrow::compute::CompareOperator compare_operator, std::string column_name, arrow::compute::Datum column_value) {
+//SelectComposite::SelectComposite(
+//        arrow::compute::CompareOperator compare_operator,
+//        std::string column_name,
+//        arrow::compute::Datum column_value,
+//        std::shared_ptr<Operator> left_child,
+//        std::shared_ptr<Operator> right_child,
+//        FilterOperator filter_operator) {
+//  compare_operator_ = compare_operator;
+//  column_name_ = std::move(column_name);
+//  column_value_ = std::move(column_value);
+//  left_child_ = left_child;
+//  right_child_ = right_child;
+//  filter_operator_ = filter_operator;
+//}
+
+Select::Select(
+        arrow::compute::CompareOperator compare_operator,
+        std::string column_name,
+        arrow::compute::Datum column_value) {
   compare_operator_ = compare_operator;
   column_name_ = std::move(column_name);
   column_value_ = std::move(column_value);
 }
 
-std::shared_ptr<Table> Select::runOperator
-(std::vector<std::shared_ptr<Table>> tables) {
-  // operator only uses first block group, ignore others
-    auto table = tables[0];
-    //
+
+arrow::compute::Datum* Select::get_filter
+(std::shared_ptr<Block> block) {
+
     arrow::Status status;
 
-    auto memory_pool = arrow::default_memory_pool();
-    arrow::compute::FunctionContext function_context(memory_pool);
+    arrow::compute::FunctionContext function_context(arrow::default_memory_pool());
     arrow::compute::CompareOptions compare_options(compare_operator_);
-    auto* filter_datum = new arrow::compute::Datum();
-    arrow::ArrayVector filter_array_vector;
+    auto* block_filter = new arrow::compute::Datum();
+
+    auto select_col = block->get_column_by_name(column_name_);
+    status = arrow::compute::Compare(&function_context,
+                                        select_col,
+                                        column_value_,
+                                        compare_options,
+                                        block_filter);
+    evaluate_status(status, __FUNCTION__, __LINE__);
+
+    return block_filter;
+}
+
+
+std::shared_ptr<arrow::ChunkedArray> Select::get_filter
+        (std::shared_ptr<Table> table) {
 
     // TODO(nicholas): Arrow currenlty doesn't supported Compare with
     //  ChunkedArray, so we just run COmpare on each block individually and
     //  combine the results into one ChunkedArray ourselves.
+    arrow::ArrayVector filter_array_vector;
+
     for (int i=0; i<table->get_num_blocks(); i++) {
-        status = arrow::compute::Compare(
-                &function_context,
-                table->get_block(i)->get_column_by_name(column_name_),
-                column_value_,
-                compare_options,
-                filter_datum
-        );
-        evaluate_status(status, __FUNCTION__, __LINE__);
-        filter_array_vector.push_back(filter_datum->make_array());
+        auto block = table->get_block(i);
+        auto* block_filter = get_filter(block);
+        filter_array_vector.push_back(block_filter->make_array());
     }
-    auto filter = std::make_shared<arrow::ChunkedArray>(filter_array_vector);
+
+    return std::make_shared<arrow::ChunkedArray>(filter_array_vector);
+
+}
+
+//    arrow::Status status;
+//
+//    std::shared_ptr<arrow::compute::Datum> left_child_filter;
+//    std::shared_ptr<arrow::compute::Datum> right_child_filter;
+//    if (left_child_ != nullptr) {
+//        left_child_filter = left_child_->get_filter(block);
+//    }
+//    if (right_child_ != nullptr) {
+//        right_child_filter = right_child_->get_filter(block);
+//    }
+//
+//    auto memory_pool = arrow::default_memory_pool();
+//    arrow::compute::FunctionContext function_context(memory_pool);
+//    arrow::compute::CompareOptions compare_options(compare_operator_);
+//    auto* block_filter = new arrow::compute::Datum();
+//
+//    // if the operator is a leaf, just perform the selction
+//    if (left_child_ == nullptr && right_child_ == nullptr) {
+//
+//        auto select_col = block->get_column_by_name
+//                (column_name_);
+//        status = arrow::compute::Compare(&function_context,
+//                                        select_col,
+//                                        column_value_,
+//                                        compare_options,
+//                                        block_filter);
+//        evaluate_status(status, __FUNCTION__, __LINE__);
+//        }
+//
+//    else {
+//        switch(filter_operator_) {
+//            case AND: {
+//                status = arrow::compute::And(&function_context, *left_child_filter,
+//                                             *right_child_filter, block_filter);
+//                evaluate_status(status, __FUNCTION__, __LINE__);
+//            }
+//            case OR: {
+//                status = arrow::compute::Or(&function_context, *left_child_filter,
+//                                            *right_child_filter, block_filter);
+//                evaluate_status(status, __FUNCTION__, __LINE__);
+//            }
+//        }
+//    }
+
+
+std::shared_ptr<Table> Select::runOperator
+(std::vector<std::shared_ptr<Table>> tables) {
+
+
+    arrow::Status status;
+    // operator only uses first table
+    auto table = tables[0];
+
+    std::shared_ptr<arrow::ChunkedArray> filter = get_filter(table);
 
     // Check if selectivity is 0 i.e. no tuples match the select predicate
     // TODO(nicholas): this is never true!
@@ -55,23 +139,26 @@ std::shared_ptr<Table> Select::runOperator
     std::vector<std::shared_ptr<arrow::ChunkedArray>> out_table_data;
     std::shared_ptr<arrow::ChunkedArray> out_col;
 
-    for (int j = 0; j < table->get_schema()->num_fields(); j++) {
+    arrow::compute::FunctionContext function_context(arrow::default_memory_pool());
+    arrow::compute::CompareOptions compare_options(compare_operator_);
+
+    for (int i = 0; i < table->get_schema()->num_fields(); i++) {
         status = arrow::compute::Filter(&function_context,
-                                        *table->get_column(j),
-                                        *filter, &out_col);
+                                        *table->get_column(i),
+                                        *filter,
+                                        &out_col);
 
         evaluate_status(status, __FUNCTION__, __LINE__);
         out_table_data.push_back(out_col);
     }
 
+    // TODO(nicholas): Throw all of this into a new Table function.
     std::vector<std::shared_ptr<arrow::ArrayData>> out_block_data;
-    int out_block_counter = 0;
     auto out_table = std::make_shared<Table>("out", table->get_schema(),
                                              BLOCK_SIZE);
 
     for (int chunk_i=0; chunk_i<out_table_data[0]->num_chunks();
          chunk_i++) {
-        int c = 0;
         for (auto &col : out_table_data) {
             out_block_data.push_back(col->chunk(chunk_i)->data());
         }
--- src/operators/Select.h
+++ src/operators/Select.h
@@ -13,18 +13,45 @@ namespace operators {
 
 class Select : public Operator{
  public:
-  Select(arrow::compute::CompareOperator compare_operator, std::string column_name, arrow::compute::Datum column_value);
+  Select(
+        arrow::compute::CompareOperator compare_operator,
+        std::string column_name,
+        arrow::compute::Datum column_value
+        );
 
-  // Operator.h
-  std::shared_ptr<Table> runOperator
-  (std::vector<std::shared_ptr<Table>> tables) override;
-  std::shared_ptr<Block> runOperatorOnBlock (std::shared_ptr<Block> block);
+    // Operator.h
+    std::shared_ptr<Table> runOperator
+    (std::vector<std::shared_ptr<Table>> tables) override;
+
+    std::shared_ptr<arrow::ChunkedArray> get_filter
+    (std::shared_ptr<Table> table);
+
+    arrow::compute::Datum* get_filter
+            (std::shared_ptr<Block> block);
+
 
 private:
   arrow::compute::CompareOperator compare_operator_;
   std::string column_name_;
   arrow::compute::Datum column_value_;
 
+};
+
+class SelectComposite : public Select{
+public:
+    SelectComposite(
+        arrow::compute::CompareOperator compare_operator,
+        std::string column_name,
+        arrow::compute::Datum column_value,
+        std::shared_ptr<Operator> left_child,
+        std::shared_ptr<Operator> right_child,
+        FilterOperator filter_operator
+    );
+
+private:
+    std::shared_ptr<Operator> left_child;
+    std::shared_ptr<Operator> right_child;
+    FilterOperator filter_operator;
 };
 
 } // namespace operators
