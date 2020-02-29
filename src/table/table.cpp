04f734ccbde59a4b43c2cf9f3ad1387e94c9c955
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-02-28 21:18:14 -0600

    Fixed bug

--- src/table/table.cpp
+++ src/table/table.cpp
@@ -240,6 +240,11 @@ int Table::get_block_row_offset(int i) const{
 }
 
 std::shared_ptr<arrow::ChunkedArray> Table::get_column(int col_index)  {
+
+    if (blocks.empty()) {
+        return nullptr;
+    }
+
     arrow::ArrayVector array_vector;
     for (int i = 0; i < blocks.size(); i++) {
         array_vector.push_back(blocks[i]->get_column(col_index));
@@ -254,6 +259,7 @@ name) {
 }
 
 std::shared_ptr<arrow::ChunkedArray> Table::get_valid_column(){
+
     arrow::ArrayVector array_vector;
     for (int i = 0; i < blocks.size(); i++) {
         array_vector.push_back(blocks[i]->get_valid_column());
