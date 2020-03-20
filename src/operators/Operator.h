8f22dc223f60aceee210f387a070d3eacf4887f1
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-03-20 05:22:10 -0500

    Added NONE to FilterOperator

--- src/operators/Operator.h
+++ src/operators/Operator.h
@@ -10,20 +10,29 @@ namespace operators {
 
 enum FilterOperator {
     AND,
-    OR
+    OR,
+    NONE
 };
 
 // TODO(nicholas): should operators return empty table or nullptr?
 class Operator {
 public:
 
-    std::shared_ptr<Operator> left_child;
-    std::shared_ptr<Operator> right_child;
-    bool is_leaf;
-
     // TODO(nicholas): abide by naming conventions
     virtual std::shared_ptr<Table> runOperator
             (std::vector<std::shared_ptr<Table>> tables) = 0;
+
+//    virtual bool is_leaf() = 0;
+
+    virtual void set_children(
+            std::shared_ptr<Operator> left_child,
+            std::shared_ptr<Operator> right_child,
+            FilterOperator filter_operator) = 0;
+
+protected:
+    std::shared_ptr<Operator> left_child_;
+    std::shared_ptr<Operator> right_child_;
+    bool is_leaf;
 };
 
 //    virtual void set_children(std::shared_ptr<Operator> left_child,
