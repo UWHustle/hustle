d8a062d60eb46b54728e91095f07f1eaac202cdd
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-03-29 17:32:30 -0500

    Removed comments

--- src/operators/Operator.h
+++ src/operators/Operator.h
@@ -14,52 +14,12 @@ enum FilterOperator {
     NONE
 };
 
-// TODO(nicholas): should operators return empty table or nullptr?
-// TODO(nicholas): Operators should accept a Field rather than a column name,
-//  since a Field would contain the column name as well as the column type.
 class Operator {
 public:
 
     virtual std::shared_ptr<Table> run_operator
-            (std::vector<std::shared_ptr<Table>> tables) = 0;
-
-//    virtual bool is_leaf() = 0;
-
-//    virtual void set_children(
-//            std::shared_ptr<Operator> left_child,
-//            std::shared_ptr<Operator> right_child,
-//            FilterOperator filter_operator) = 0;
-
-//protected:
-//    std::shared_ptr<Operator> left_child_;
-//    std::shared_ptr<Operator> right_child_;
-//    bool is_leaf;
+    (std::vector<std::shared_ptr<Table>> tables) = 0;
 };
-
-//    virtual void set_children(std::shared_ptr<Operator> left_child,
-//            std::shared_ptr<Operator> right_child) = 0;
-//protected:
-//    std::shared_ptr<Operator> left_child_;
-//    std::shared_ptr<Operator> right_child_;
-//    FilterOperator filter_operator_;
-
-
-//class OperatorLeaf : public Operator {
-//    public:
-//
-//    virtual std::shared_ptr<Table> runOperator
-//            (std::vector<std::shared_ptr<Table>> tables) = 0;
-//
-//};
-//
-//class OperatorComposite : public Operator {
-//    public:
-//
-//    std::shared_ptr<Operator> left_child;
-//    std::shared_ptr<Operator> right_child;
-//};
-
-
 } // namespace operators
 } // namespace hustle
 
