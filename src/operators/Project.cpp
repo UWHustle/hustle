f760bef4ff77dbc57b2f1e741526d6931fff1461
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-03-29 13:08:49 -0500

    Projection works for index array inputs

--- src/operators/Project.cpp
+++ src/operators/Project.cpp
@@ -3,3 +3,72 @@
 //
 
 #include "Project.h"
+#include "../table/util.h"
+
+hustle::operators::Projection::Projection(
+        std::vector<ProjectionUnit> projection_units) {
+
+}
+
+std::shared_ptr<Table> hustle::operators::Projection::Project(
+        std::vector<ProjectionUnit> projection_units) {
+
+    arrow::Status status;
+
+    arrow::SchemaBuilder schema_builder;
+
+    arrow::compute::FunctionContext function_context(
+            arrow::default_memory_pool());
+    arrow::compute::TakeOptions take_options;
+    std::shared_ptr<arrow::ChunkedArray> out_col;
+
+    std::vector<std::shared_ptr<arrow::ChunkedArray>> out_table_data;
+    // TODO(nicholas): for now, assume that the selections are always arrays
+    //  of indices, not filters.
+    for (auto &unit : projection_units) {
+
+        auto table = unit.table;
+        auto selection = unit.selection;
+        auto fields = unit.fields;
+
+        status = schema_builder.AddFields(fields);
+        evaluate_status(status, __PRETTY_FUNCTION__, __LINE__);
+
+        for (auto &field : fields) {
+            auto col = table->get_column_by_name(field->name());
+
+            status = arrow::compute::Take(&function_context,
+                                          *col,
+                                          *selection.make_array(),
+                                          take_options,
+                                          &out_col);
+            evaluate_status(status, __PRETTY_FUNCTION__, __LINE__);
+
+            out_table_data.push_back(out_col);
+        }
+    }
+
+    status = schema_builder.Finish().status();
+    evaluate_status(status,__PRETTY_FUNCTION__, __LINE__);
+    auto out_schema = schema_builder.Finish().ValueOrDie();
+
+    auto out_table = std::make_shared<Table>("out", out_schema, BLOCK_SIZE);
+
+
+    std::vector<std::shared_ptr<arrow::ArrayData>> out_block_data;
+
+    for (int chunk_i=0; chunk_i<out_table_data[0]->num_chunks(); chunk_i++) {
+        for (auto &col : out_table_data) {
+            out_block_data.push_back(col->chunk(chunk_i)->data());
+        }
+        out_table->insert_records(out_block_data);
+        out_block_data.clear();
+    }
+
+    return out_table;
+}
+
+std::shared_ptr<Table> hustle::operators::Projection::run_operator(
+        std::vector<std::shared_ptr<Table>> tables) {
+    return std::shared_ptr<Table>();
+}
--- src/operators/Project.h
+++ src/operators/Project.h
@@ -6,6 +6,7 @@
 #define HUSTLE_PROJECT_H
 #include <arrow/api.h>
 #include <table/table.h>
+#include <arrow/compute/api.h>
 #include "Operator.h"
 
 
@@ -13,11 +14,20 @@
 namespace hustle {
 namespace operators {
 
-class Project : public Operator {
+struct ProjectionUnit {
+    std::shared_ptr<Table> table;
+    arrow::compute::Datum selection;
+    std::vector<std::shared_ptr<arrow::Field>> fields;
+};
+
+class Projection : public Operator {
 public:
 
-    Project(std::shared_ptr<arrow::Schema> schema);
+    Projection(std::vector<ProjectionUnit> projection_units);
+    std::shared_ptr<Table> Project(std::vector<ProjectionUnit> projection_units);
 
+    std::shared_ptr<Table> run_operator
+            (std::vector<std::shared_ptr<Table>> tables);
 
 };
 
