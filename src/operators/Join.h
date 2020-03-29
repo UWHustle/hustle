13156fc7feae8e2bb22227bd5c2b96dc5e97b1ca
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-03-29 12:29:02 -0500

    Hash table stores global indices; reorganizing everything

--- src/operators/Join.h
+++ src/operators/Join.h
@@ -29,6 +29,9 @@ public:
     std::shared_ptr<Table> run_operator
     (std::vector<std::shared_ptr<Table>> table) override;
 
+    std::unordered_map<int64_t, int64_t> build_hash_table
+            (std::shared_ptr<Table>table);
+
     /**
     * Perform a natural join on two tables using hash join. Projections are not
     * yet supported; all columns from both tables will be returned in the
@@ -44,9 +47,16 @@ public:
             const std::shared_ptr<Table>& right_table,
             const arrow::compute::Datum& right_selection);
 
+    std::shared_ptr<Table> hash_join2(
+            const std::shared_ptr<Table>& left_table,
+            const arrow::compute::Datum& left_selection,
+            const std::shared_ptr<Table>& right_table,
+            const arrow::compute::Datum& right_selection);
+
     arrow::compute::Datum get_left_indices();
     arrow::compute::Datum get_right_indices();
-    arrow::compute::Datum get_indices_for_table(std::shared_ptr<Table> other);
+    arrow::compute::Datum get_indices_for_table(
+            const std::shared_ptr<Table> &other);
 
 private:
     std::shared_ptr<Table> left_table_;
@@ -58,6 +68,9 @@ private:
     std::shared_ptr<arrow::Array> left_indices_;
     std::shared_ptr<arrow::Array> right_indices_;
 
+    std::unordered_map<int64_t, int64_t>
+    build_hash_table(std::shared_ptr<Table> right_table,
+                     arrow::compute::Datum right_selection);
 };
 
 } // namespace operators
