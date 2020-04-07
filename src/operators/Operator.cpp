a520b351bc2eabd101e0bac5d9852458e335c355
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-04-07 17:11:55 -0500

    Added OperatorResultUnit

--- src/operators/Operator.cpp
+++ src/operators/Operator.cpp
@@ -9,6 +9,19 @@
 namespace hustle {
 namespace operators {
 
+    OperatorResultUnit::OperatorResultUnit(
+        std::shared_ptr<Table> table,
+        std::string col_name,
+        std::shared_ptr<arrow::ChunkedArray> join_col,
+        arrow::compute::Datum filter,
+        arrow::compute::Datum selection) {
+
+        table_ = table;
+        col_name_ = col_name;
+        filter_ = filter;
+        selection_ = selection;
+    }
+
     ResultType OperatorResult::get_type() {
         return type_;
     }
@@ -18,9 +31,17 @@ namespace operators {
         type_ = JOIN;
     }
 
+//    arrow::compute::Datum JoinResult::get_filter() {
+//        return
+//    }
+
     SelectResult::SelectResult(arrow::compute::Datum filter) {
         filter_ = filter;
         type_ = SELECT;
     }
+
+//    arrow::compute::Datum SelectResult::get_indices() {
+//        return arrow::compute::Datum();
+//    }
 }
 }
--- src/operators/Operator.h
+++ src/operators/Operator.h
@@ -27,16 +27,18 @@ struct JoinResultColumn {
     // TODO(nicholas): Combine into a ColumnReference?
     std::shared_ptr<Table> table;
     std::string join_col_name;
-    arrow::compute::Datum filter; // filters are ChunkedArrays
-    arrow::compute::Datum selection; // selections are Arrays
     // column AFTER a filter from a select predicate was applied.
     std::shared_ptr<arrow::ChunkedArray> join_col;
+    arrow::compute::Datum filter; // filters are ChunkedArrays
+    arrow::compute::Datum selection; // selections are Arrays
 };
-
 
 class OperatorResult {
 public:
     virtual ~OperatorResult() = default;
+//    virtual arrow::compute::Datum get_filter() = 0;
+//    virtual arrow::compute::Datum get_indices() = 0;
+
     ResultType get_type();
 protected:
     ResultType type_;
@@ -45,6 +47,9 @@ protected:
 class SelectResult : public OperatorResult {
 public:
     SelectResult(arrow::compute::Datum filter);
+
+//    arrow::compute::Datum get_filter() override ;
+//    arrow::compute::Datum get_indices() override ;
 
     arrow::compute::Datum filter_;
 private:
@@ -55,17 +60,13 @@ class JoinResult : public OperatorResult {
 public:
     JoinResult(std::vector<JoinResultColumn> join_results);
 
+//    arrow::compute::Datum get_filter() override ;
+//    arrow::compute::Datum get_indices() override ;
+
     std::vector<JoinResultColumn> join_results_;
 private:
 
 };
-
-
-
-
-
-
-
 
 struct ColumnReference {
     std::shared_ptr<Table> table;
@@ -77,6 +78,32 @@ class Operator {
 public:
 
 };
+
+class OperatorResultUnit {
+
+public:
+
+    OperatorResultUnit(
+            std::shared_ptr<Table> table,
+            std::string col_name,
+            std::shared_ptr<arrow::ChunkedArray> join_col,
+            arrow::compute::Datum filter,
+            arrow::compute::Datum selection
+    );
+
+    // TODO(nicholas): Combine into a ColumnReference?
+    std::shared_ptr<Table> table_;
+    std::string col_name_;
+    // column AFTER a filter from a select predicate was applied.
+    std::shared_ptr<arrow::ChunkedArray> join_col;
+    arrow::compute::Datum filter_; // filters are ChunkedArrays
+    arrow::compute::Datum selection_; // selections are Arrays
+
+private:
+
+};
+
+
 } // namespace operators
 } // namespace hustle
 
