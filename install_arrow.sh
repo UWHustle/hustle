298d9777b063c3a44e081a81a4ac7ba36888bf3c
Yannis Chronis <chronis@cs.wisc.edu>
2020-04-13 15:49:52 -0500

    update install script and circle ci

--- install_arrow.sh
+++ install_arrow.sh
@@ -1,18 +1,7 @@
-sudo apt update
-sudo apt install -y -V apt-transport-https gnupg lsb-release wget
-sudo wget -O /usr/share/keyrings/apache-arrow-keyring.gpg https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/apache-arrow-keyring.gpg
-sudo tee /etc/apt/sources.list.d/apache-arrow.list <<APT_LINE
-deb [arch=amd64 signed-by=/usr/share/keyrings/apache-arrow-keyring.gpg] https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/ $(lsb_release --codename --short) main
-deb-src [signed-by=/usr/share/keyrings/apache-arrow-keyring.gpg] https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/ $(lsb_release --codename --short) main
-APT_LINE
-sudo apt update
-sudo apt install -y -V libarrow-dev # For C++
-sudo apt install -y -V libarrow-glib-dev # For GLib (C)
-sudo apt install -y -V libarrow-flight-dev # For Flight C++
-sudo apt install -y -V libplasma-dev # For Plasma C++
-sudo apt install -y -V libplasma-glib-dev # For Plasma GLib (C)
-sudo apt install -y -V libgandiva-dev # For Gandiva C++
-sudo apt install -y -V libgandiva-glib-dev # For Gandiva GLib (C)
-sudo apt install -y -V libparquet-dev # For Apache Parquet C++
-sudo apt install -y -V libparquet-glib-dev # For Apache Parquet GLib (C)
+git clone https://github.com/apache/arrow.git
+cd arrow/cpp
+mkdir release
+cd release
+cmake -DARROW_COMPUTE=ON -DARROW_CSV=ON ..
+sudo make install -j4
 
--- install_requirements.sh
+++ install_requirements.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 if [[ `uname` == "Darwin" ]]; then
-		brew update && brew bundle --file=Brewfile
+                brew update && brew bundle --file=Brewfile
 elif [[ `uname` == "Linux" ]]; then
-		wget https://cmake.org/files/v3.15/cmake-3.15.5.tar.gz
-		tar -xzvf cmake-3.15.5.tar.gz
-		rm -f cmake-3.15.5.tar.gz
-		cd cmake-3.15.5 && ./bootstrap && make -j 6 && sudo make install
-		rm -rf cmake-3.15.5
-		sudo add-apt-repository ppa:ubuntu-toolchain-r/test --yes
-		sudo apt-get update
-		sudo apt-get install gcc-9 g++-9 --yes
-		sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-9
+                wget https://cmake.org/files/v3.15/cmake-3.15.5.tar.gz
+                tar -xzvf cmake-3.15.5.tar.gz
+                rm -f cmake-3.15.5.tar.gz
+                cd cmake-3.15.5 && ./bootstrap && make -j 6 && sudo make install
+                rm -rf cmake-3.15.5
+                sudo apt-get install software-properties-common --yes
+                sudo add-apt-repository ppa:ubuntu-toolchain-r/test --yes
+                sudo apt-get update
+                sudo apt-get install gcc-9 g++-9 --yes
+                sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-9
 fi
