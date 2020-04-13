ea2c0d455f7984e828d529edcbbad2a10da5e44b
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-04-13 18:38:52 -0500

    Comments

--- src/operators/Predicate.cpp
+++ src/operators/Predicate.cpp
@@ -3,3 +3,4 @@
 //
 
 #include "Predicate.h"
+
--- src/operators/Predicate.h
+++ src/operators/Predicate.h
@@ -7,8 +7,17 @@
 
 #include <arrow/api.h>
 #include <arrow/compute/api.h>
+#include "Operator.h"
 
+namespace hustle {
+namespace operators {
+
+
+// JoinPredicate vs SelectPredicate? One has another column as the RHS, the
+// other has a literal.
 struct Predicate {
+    // The LHS side of the predicate (i.e. the column) will be provided
+    // inside the operator.
     ColumnReference col_ref;
     arrow::compute::CompareOperator comparator;
     arrow::compute::Datum value;
@@ -22,13 +31,19 @@ struct PredicateNode {
 
 };
 
-class Predicate {
+class PredicateTree {
 
 public:
 
     std::shared_ptr<PredicateNode> root;
 
 };
+
+
+}
+}
+
+
 
 
 
