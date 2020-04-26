98ed4ff512d6368e07cfa8b489d613e6622aed75
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-04-26 09:36:11 -0500

    Added doc comments

--- src/operators/Select.h
+++ src/operators/Select.h
@@ -11,26 +11,59 @@
 
 namespace hustle::operators {
 
-    //TODO(nicholas): Rename SelectComposite to Select and Select to
-    // SelectNode. Select will internally construct the tree, so Jerry
-    // doesn't have to do it himself. He could just pass in a vector of
-    // SelectPredicates. But how to handle ANDing/ORing of filters?
-
 class Select : public Operator{
 public:
 
+    /**
+     * Construct a Select operator.
+     *
+     * @param prev_result OperatorResult from an upstream operator
+     * @param tree predicate tree
+     */
     Select(std::shared_ptr<OperatorResult> prev_result,
             std::shared_ptr<PredicateTree> tree);
 
+    /**
+     * Perform the selection specified by the predicate tree passed into the
+     * constructor.
+     *
+     * @return a new OperatorResult with an updated filter.
+     */
     std::shared_ptr<OperatorResult> run() override;
-    arrow::compute::Datum get_filter(const std::shared_ptr<Node>& node,
-                                const std::shared_ptr<Block>& block);
+
 
 private:
     std::shared_ptr<PredicateTree> tree_;
     std::shared_ptr<OperatorResult> prev_result_;
     std::shared_ptr<Table> table_;
 
+    /**
+     * Perform the selection specified by a node in the predicate tree on
+     * one block of the table. If the node is a not a leaf node, this
+     * function will be recursively called on its children. The nodes of the
+     * predicate tree are visited using inorder traversal.
+     *
+     * @param node A node of the predicate tree.
+     * @param block A block of the table
+     *
+     * @return A filter corresponding to values that satisfy the node's
+     * selection predicate(s)
+     */
+    arrow::compute::Datum get_filter(const std::shared_ptr<Node>& node,
+                                     const std::shared_ptr<Block>& block);
+
+    /**
+     * Perform the selection specified by a predicate (i.e. leaf node) in the
+     * predicate tree on one block of the table. This is the base function
+     * call of the other get_filter() function.
+     *
+     * @param predicate A predicate from one of the leaf nodes of the
+     * predicate tree.
+     * @param block A block of the table
+     *
+     * @return A filter corresponding to values that satisfy the node's
+     * selection predicate(s)
+     */
     arrow::compute::Datum get_filter(
             const std::shared_ptr<Predicate>& predicate,
             const std::shared_ptr<Block>& block );
