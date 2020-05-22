b3df99fcd7e9f061b460ac6655bf8823b080e1f6
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-05-22 17:53:39 -0500

    Add empty BloomFilter test

---
+++ src/utils/tests/BloomFilterTest.cpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+
+#include <fstream>
+
+using namespace testing;
+
+class JoinTestFixture : public testing::Test {
+protected:
+
+    void SetUp() override {
+
+    }
+};
+
+
+TEST_F(JoinTestFixture, EquiJoin1) {
+
+}
