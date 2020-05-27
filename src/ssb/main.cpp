dc2ea3ce91dab592de7ec52e952d2a093fed4cda
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-05-27 17:59:10 -0500

    Added one LIP query

--- src/ssb/main.cpp
+++ src/ssb/main.cpp
@@ -6,21 +6,24 @@ int main(int argc, char *argv[]) {
 
     SSB workload;
 
-    workload.q11();
-    workload.q12();
-    workload.q13();
+//    workload.q11();
+//    workload.q12();
+//    workload.q13();
+//
+//    workload.q21();
+//    workload.q22();
+//    workload.q23();
+//
+//    workload.q31();
+//    workload.q32();
+//    workload.q33();
+//    workload.q34();
 
-    workload.q21();
-    workload.q22();
-    workload.q23();
+//    workload.q41();
+//    workload.q42();
+//    workload.q43();
 
-    workload.q31();
-    workload.q32();
-    workload.q33();
-    workload.q34();
+    workload.q41_lip();
 
-    workload.q41();
-    workload.q42();
-    workload.q43();
 
 }
