2a4b0786b56a9917af89a8f54d71a53b80d25248
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-06-24 20:04:47 -0500

    Start parallel probe -- commented out until I fix typing issue

--- src/operators/Join.cpp
+++ src/operators/Join.cpp
@@ -5,6 +5,7 @@
 #include <table/util.h>
 #include <iostream>
 #include <arrow/scalar.h>
+//#include <arrow/compute/api_vector.h>
 
 namespace hustle::operators {
 
@@ -63,13 +64,43 @@ void Join::probe_hash_table_block
         int num_joined_indices = 0;
         auto chunk = probe_col->chunk(i);
         auto chunk_length = chunk->length();
+        
+        arrow::Datum match_result;
+        arrow::Datum not_null;
+        arrow::Datum left_joined;
+        arrow::Datum right_joined;
+        arrow::Datum offset;
+
+        // NOTE: This only makes sense if right_join_col_ does not contain duplicate values.
+//        status = arrow::compute::Match(chunk, right_join_col_).Value(&match_result);
+//        evaluate_status(status, __PRETTY_FUNCTION__, __LINE__);
+////        std::cout << match_result.make_array()->ToString() << std::endl;
+////
+//        status = arrow::compute::IsNull(match_result).Value(&not_null);
+//        evaluate_status(status, __PRETTY_FUNCTION__, __LINE__);
+//        status = arrow::compute::Invert(not_null).Value(&not_null);
+//        evaluate_status(status, __PRETTY_FUNCTION__, __LINE__);
+//
+//        status = arrow::compute::Filter(match_result, not_null).Value(&right_joined);
+////        std::cout << right_joined.make_array()->ToString() << std::endl;
+//
+//        status = arrow::compute::internal::GetTakeIndices(*not_null.array(), arrow::compute::FilterOptions::EMIT_NULL).Value(&left_joined);
+//        evaluate_status(status, __PRETTY_FUNCTION__, __LINE__);
+//
+//        arrow::Datum temp;
+//        auto s = arrow::Datum((int64_t) chunk_row_offsets[i]);
+//
+//        status = arrow::compute::Add(left_joined, s).Value(&left_joined);
+//        evaluate_status(status, __PRETTY_FUNCTION__, __LINE__);
+//        arrow::Datum left_indices;
+//        status = arrow::compute::Take(match_result, temp).Value(&left_indices);
+//                std::cout << left_indices.make_array()->ToString() << std::endl;
 
         // The indices of the rows joined in chunk i
         int64_t joined_left_indices[chunk_length];
         int64_t joined_right_indices[chunk_length];
 
         // TODO(nicholas): for now, we assume the join column is fixed width type, i.e. values are stored in the buffer at index 1.
-
         auto left_join_chunk_data = chunk->data()->GetValues<int64_t>(1, 0);
 
         for (int row = 0; row < chunk_length; row++) {
@@ -79,8 +110,16 @@ void Join::probe_hash_table_block
                 joined_left_indices[num_joined_indices] = chunk_row_offsets[i] + row;  // insert left row index
                 joined_right_indices[num_joined_indices] = key_value_pair->second; // insert right row index
                 ++num_joined_indices;
+
+//                std::cout << chunk_row_offsets[i] + row << std::endl;
             }
         }
+
+//        auto l = left_joined.array()->GetValues<int64_t>(1, 0);
+//        auto r = right_joined.array()->GetValues<int64_t>(1, 0);
+//
+//        new_left_indices_vector[i] = std::vector<int64_t>(l, l+left_joined.length());
+//        new_right_indices_vector[i] = std::vector<int64_t>(r, r+right_joined.length());
 
         new_left_indices_vector[i] = std::vector<int64_t>(joined_left_indices, joined_left_indices + num_joined_indices);
         new_right_indices_vector[i] = std::vector<int64_t>(joined_right_indices, joined_right_indices + num_joined_indices);
