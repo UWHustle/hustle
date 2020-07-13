02776b27e525944502cfa119085718699b76d526
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-07-13 00:36:34 -0500

    Simplify num_cells_

--- src/utils/BloomFilter.h
+++ src/utils/BloomFilter.h
@@ -21,8 +21,7 @@ public:
         eps_ = 1e-2;
 
         int n = num_vals;
-        num_hash_ = int (round( - log(eps_) / log(2)));
-        num_cells_ = int (n * num_hash_ / log(2));
+        num_cells_ = n * 100;
         int num_bytes_ = sizeof(uint8_t) * num_cells_ / 8 + 1;
 
         num_hash_ = 3;
