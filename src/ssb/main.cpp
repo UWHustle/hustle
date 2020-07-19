a80d6f2971224407595557168bbd810165ad7f32
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-07-19 08:23:36 -0500

    Use smaller datatypes in lo schema; Fix testing loop

--- src/ssb/main.cpp
+++ src/ssb/main.cpp
@@ -9,7 +9,7 @@ void read_from_csv() {
     std::shared_ptr<arrow::Schema> lo_schema, c_schema, s_schema, p_schema, d_schema;
 
 
-    auto field1 = arrow::field("order key", arrow::int64());
+    auto field1 = arrow::field("order key", arrow::uint32());
     auto field2 = arrow::field("line number", arrow::int64());
     auto field3 = arrow::field("cust key", arrow::int64());
     auto field4 = arrow::field("part key", arrow::int64());
@@ -17,10 +17,10 @@ void read_from_csv() {
     auto field6 = arrow::field("order date", arrow::int64());
     auto field7 = arrow::field("ord priority", arrow::utf8());
     auto field8 = arrow::field("ship priority", arrow::int64());
-    auto field9 = arrow::field("quantity", arrow::int64());
+    auto field9 = arrow::field("quantity", arrow::uint8());
     auto field10 = arrow::field("extended price", arrow::int64());
     auto field11 = arrow::field("ord total price", arrow::int64());
-    auto field12 = arrow::field("discount", arrow::int64());
+    auto field12 = arrow::field("discount", arrow::uint8());
     auto field13 = arrow::field("revenue", arrow::int64());
     auto field14 = arrow::field("supply cost", arrow::int64());
     auto field15 = arrow::field("tax", arrow::int64());
@@ -146,71 +146,104 @@ void read_from_csv() {
             p_field9});
 
 
-
-    auto t = read_from_csv_file("/Users/corrado/hustle/src/ssb/data/ssb-01/customer.tbl", c_schema, BLOCK_SIZE);
+    std::shared_ptr<Table> t;
+    t = read_from_csv_file("/Users/corrado/hustle/src/ssb/data/ssb-01/customer.tbl", c_schema, BLOCK_SIZE);
     write_to_file("/Users/corrado/hustle/src/ssb/data/ssb-01/customer.hsl", *t);
     std::cout << "c" << std::endl;
+//
+//    t = read_from_csv_file("/Users/corrado/hustle/src/ssb/data/ssb-01-10MB/supplier.tbl", s_schema, BLOCK_SIZE);
+//    write_to_file("/Users/corrado/hustle/src/ssb/data/ssb-01/supplier.hsl", *t);
+//    std::cout << "s" << std::endl;
+//
+//    t = read_from_csv_file("/Users/corrado/hustle/src/ssb/data/ssb-01-10MB/date.tbl", d_schema, BLOCK_SIZE);
+//    write_to_file("/Users/corrado/hustle/src/ssb/data/ssb-01/date.hsl", *t);
+//    std::cout << "d" << std::endl;
+//
+//    t = read_from_csv_file("/Users/corrado/hustle/src/ssb/data/ssb-01-10MB/part.tbl", p_schema, BLOCK_SIZE);
+//    write_to_file("/Users/corrado/hustle/src/ssb/data/ssb-01/part.hsl", *t);
+//    std::cout << "p" << std::endl;
 
-    t = read_from_csv_file("/Users/corrado/hustle/src/ssb/data/ssb-01/supplier.tbl", s_schema, BLOCK_SIZE);
-    write_to_file("/Users/corrado/hustle/src/ssb/data/ssb-01/supplier.hsl", *t);
-    std::cout << "s" << std::endl;
+//    t = read_from_csv_file("/Users/corrado/hustle/src/ssb/data/ssb-01/lineorder.tbl", lo_schema, BLOCK_SIZE);
+//    write_to_file("/Users/corrado/hustle/src/ssb/data/ssb-01/lineorder.hsl", *t);
+//    std::cout << "lo" << std::endl;
 
-    t = read_from_csv_file("/Users/corrado/hustle/src/ssb/data/ssb-01/lineorder.tbl", lo_schema, BLOCK_SIZE);
-    write_to_file("/Users/corrado/hustle/src/ssb/data/ssb-01/lineorder.hsl", *t);
-    std::cout << "lo" << std::endl;
-
-    t = read_from_csv_file("/Users/corrado/hustle/src/ssb/data/ssb-01/date.tbl", d_schema, BLOCK_SIZE);
-    write_to_file("/Users/corrado/hustle/src/ssb/data/ssb-01/date.hsl", *t);
-    std::cout << "d" << std::endl;
-
-    t = read_from_csv_file("/Users/corrado/hustle/src/ssb/data/ssb-01/part.tbl", p_schema, BLOCK_SIZE);
-    write_to_file("/Users/corrado/hustle/src/ssb/data/ssb-01/part.hsl", *t);
-    std::cout << "p" << std::endl;
-
+//    auto t = read_from_csv_file("/Users/corrado/hustle/src/ssb/data/ssb-01/customer.tbl", c_schema, 20*BLOCK_SIZE);
+//    write_to_file("/Users/corrado/hustle/src/ssb/data/ssb-01-20MB/customer.hsl", *t);
+//    std::cout << "c" << std::endl;
+//
+//    t = read_from_csv_file("/Users/corrado/hustle/src/ssb/data/ssb-01/supplier.tbl", s_schema, 20*BLOCK_SIZE);
+//    write_to_file("/Users/corrado/hustle/src/ssb/data/ssb-01-20MB/supplier.hsl", *t);
+//    std::cout << "s" << std::endl;
+//
+//    t = read_from_csv_file("/Users/corrado/hustle/src/ssb/data/ssb-01/date.tbl", d_schema, 20*BLOCK_SIZE);
+//    write_to_file("/Users/corrado/hustle/src/ssb/data/ssb-01-20MB/date.hsl", *t);
+//    std::cout << "d" << std::endl;
+//
+//    t = read_from_csv_file("/Users/corrado/hustle/src/ssb/data/ssb-01/part.tbl", p_schema, 20*BLOCK_SIZE);
+//    write_to_file("/Users/corrado/hustle/src/ssb/data/ssb-01-20MB/part.hsl", *t);
+//    std::cout << "p" << std::endl;
+//
+//    t = read_from_csv_file("/Users/corrado/hustle/src/ssb/data/ssb-10/lineorder.tbl", lo_schema, 20*BLOCK_SIZE);
+//    write_to_file("/Users/corrado/hustle/src/ssb/data/ssb-10-20MB/lineorder.hsl", *t);
+//    std::cout << "lo" << std::endl;
 }
-
 
 int main(int argc, char *argv[]) {
 
 //    read_from_csv();
+//    return 0;
 
-    SSB workload(1, false);
-
-    auto t1 = high_resolution_clock::now();
-
-    workload.q11();
-    workload.q12();
-    workload.q13();
-
-    workload.q21();
-    workload.q22();
-    workload.q23();
-
-    workload.q31();
-    workload.q32();
-    workload.q33();
-    workload.q34();
-
-    workload.q41();
-    workload.q42();
-    workload.q43();
-
-    auto t2 = high_resolution_clock::now();
-    std::cout << duration_cast<milliseconds>(t2-t1).count() << std::endl;
-//    workload.q11_lip();
-//    workload.q12_lip();
-//    workload.q13_lip();
+//    SSB workload(1, true);
+//        workload.c->get_block(0)->print();
+//    workload.lo->get_block(0)->print();
+//    SSB workload(1, false);
+    SSB workload(10, false);
+    std::cout << workload.lo->get_num_blocks() << std::endl;
+    std::string input;
+    std::cout << ">> ";
+    std::getline(std::cin, input);
+    while (input != "exit") {
+        for (int i = 0; i < 1; i++) {
+            auto t1 = high_resolution_clock::now();
+//            workload.q11();
+//            workload.q12();
+//            workload.q13();
 //
-//    workload.q21_lip();
-//    workload.q22_lip();
-//    workload.q23_lip();
+//            workload.q21();
+//            workload.q22();
+//            workload.q23();
 //
-//    workload.q31_lip();
-//    workload.q32_lip();
-//    workload.q33_lip();
-//    workload.q34_lip();
+//            workload.q31();
+//            workload.q32();
+//            workload.q33();
+//            workload.q34();
 //
-//    workload.q41_lip();
-//    workload.q42_lip();
-//    workload.q43_lip();
+//            workload.q41();
+//            workload.q42();
+//            workload.q43();
+//
+//            workload.q11_lip();
+//            workload.q12_lip();
+//            workload.q13_lip();
+
+//            workload.q21_lip();
+            workload.q22_lip();
+//            workload.q23_lip();
+//
+//            workload.q31_lip();
+//            workload.q32_lip();
+//            workload.q33_lip();
+//            workload.q34_lip();
+//
+//            workload.q41_lip();
+//            workload.q42_lip();
+//            workload.q43_lip();
+
+            auto t2 = high_resolution_clock::now();
+            std::cout << duration_cast<milliseconds>(t2 - t1).count() << std::endl;
+
+            std::cout << ">> ";
+            std::getline(std::cin, input);
+        }
+    }
 }
