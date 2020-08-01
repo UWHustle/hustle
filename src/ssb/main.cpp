9042018d9f1af256870e1ab2cfad71718a8627c1
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-08-01 10:02:28 -0500

    Simplified schema definitions

--- src/ssb/main.cpp
+++ src/ssb/main.cpp
@@ -1,5 +1,6 @@
 #include "ssb_workload.h"
 #include "../table/util.h"
+#include "skew.h"
 using namespace hustle::operators;
 using namespace std::chrono;
 
@@ -8,7 +9,7 @@ void read_from_csv() {
     std::shared_ptr <Table> lo, c, s, p, d;
     std::shared_ptr <arrow::Schema> lo_schema, c_schema, s_schema, p_schema, d_schema;
 
-    auto field1 = arrow::field("order key", arrow::uint32());
+    auto field1 = arrow::field("order key", arrow::int64());
     auto field2 = arrow::field("line number", arrow::int64());
     auto field3 = arrow::field("cust key", arrow::int64());
     auto field4 = arrow::field("part key", arrow::int64());
@@ -31,113 +32,63 @@ void read_from_csv() {
                              field16,field17});
 
 
-    std::shared_ptr<arrow::Field>s_field1=arrow::field("s supp key",
-                                                       arrow::int64());
-    std::shared_ptr<arrow::Field>s_field2=arrow::field("s name",
-                                                       arrow::utf8());
-    std::shared_ptr<arrow::Field>s_field3=arrow::field("s address",
-                                                       arrow::utf8());
-    std::shared_ptr<arrow::Field>s_field4=arrow::field("s city",
-                                                       arrow::utf8());
-    std::shared_ptr<arrow::Field>s_field5=arrow::field("s nation",
-                                                       arrow::utf8());
-    std::shared_ptr<arrow::Field>s_field6=arrow::field("s region",
-                                                       arrow::utf8());
-    std::shared_ptr<arrow::Field>s_field7=arrow::field("s phone",
-                                                       arrow::utf8());
+    auto s_field1=arrow::field("s supp key",arrow::int64());
+    auto s_field2=arrow::field("s name",arrow::utf8());
+    auto s_field3=arrow::field("s address", arrow::utf8());
+    auto s_field4=arrow::field("s city",arrow::utf8());
+    auto s_field5=arrow::field("s nation",arrow::utf8());
+    auto s_field6=arrow::field("s region", arrow::utf8());
+    auto s_field7=arrow::field("s phone",arrow::utf8());
 
     s_schema=arrow::schema({s_field1,s_field2,s_field3,s_field4,
                             s_field5,
                             s_field6,s_field7});
 
 
-    std::shared_ptr<arrow::Field>c_field1=arrow::field("c cust key",
-                                                       arrow::int64());
-    std::shared_ptr<arrow::Field>c_field2=arrow::field("c name",
-                                                       arrow::utf8());
-    std::shared_ptr<arrow::Field>c_field3=arrow::field("c address",
-                                                       arrow::utf8());
-    std::shared_ptr<arrow::Field>c_field4=arrow::field("c city",
-                                                       arrow::utf8());
-    std::shared_ptr<arrow::Field>c_field5=arrow::field("c nation",
-                                                       arrow::utf8());
-    std::shared_ptr<arrow::Field>c_field6=arrow::field("c region",
-                                                       arrow::utf8());
-    std::shared_ptr<arrow::Field>c_field7=arrow::field("c phone",
-                                                       arrow::utf8());
-    std::shared_ptr<arrow::Field>c_field8=arrow::field("c mkt segment",
-                                                       arrow::utf8());
+    auto c_field1=arrow::field("c cust key",arrow::int64());
+    auto c_field2=arrow::field("c name",arrow::utf8());
+    auto c_field3=arrow::field("c address", arrow::utf8());
+    auto c_field4=arrow::field("c city", arrow::utf8());
+    auto c_field5=arrow::field("c nation", arrow::utf8());
+    auto c_field6=arrow::field("c region",arrow::utf8());
+    auto c_field7=arrow::field("c phone",arrow::utf8());
+    auto c_field8=arrow::field("c mkt segment",arrow::utf8());
 
     c_schema=arrow::schema({c_field1,c_field2,c_field3,c_field4,
                             c_field5,c_field6,c_field7,c_field8});
 
-    std::shared_ptr<arrow::Field>d_field1=arrow::field("date key",
-                                                       arrow::int64());
-    std::shared_ptr<arrow::Field>d_field2=arrow::field("date",
-                                                       arrow::utf8());
-    std::shared_ptr<arrow::Field>d_field3=arrow::field("day of week",
-                                                       arrow::utf8());
-    std::shared_ptr<arrow::Field>d_field4=arrow::field("month",
-                                                       arrow::utf8());
-    std::shared_ptr<arrow::Field>d_field5=arrow::field("year",
-                                                       arrow::int64());
-    std::shared_ptr<arrow::Field>d_field6=arrow::field("year month num",
-                                                       arrow::int64());
-    std::shared_ptr<arrow::Field>d_field7=arrow::field("year month",
-                                                       arrow::utf8());
-    std::shared_ptr<arrow::Field>d_field8=arrow::field("day num in week",
-                                                       arrow::int64());
-    std::shared_ptr<arrow::Field>d_field9=arrow::field("day num in "
-                                                       "month",
-                                                       arrow::int64());
-    std::shared_ptr<arrow::Field>d_field10=arrow::field("day num in "
-                                                        "year",
-                                                        arrow::int64());
-    std::shared_ptr<arrow::Field>d_field11=arrow::field("month num in "
-                                                        "year",
-                                                        arrow::int64());
-    std::shared_ptr<arrow::Field>d_field12=arrow::field("week num in "
-                                                        "year",
-                                                        arrow::int64());
-    std::shared_ptr<arrow::Field>d_field13=arrow::field("selling season",
-                                                        arrow::utf8());
-    std::shared_ptr<arrow::Field>d_field14=arrow::field("last day in "
-                                                        "week fl",
-                                                        arrow::int64());
-    std::shared_ptr<arrow::Field>d_field15=arrow::field("last day in "
-                                                        "month fl",
-                                                        arrow::int64());
-    std::shared_ptr<arrow::Field>d_field16=arrow::field("holiday fl",
-                                                        arrow::int64());
-    std::shared_ptr<arrow::Field>d_field17=arrow::field("weekday fl",
-                                                        arrow::int64());
+    auto d_field1=arrow::field("date key", arrow::int64());
+    auto d_field2=arrow::field("date", arrow::utf8());
+    auto d_field3=arrow::field("day of week", arrow::utf8());
+    auto d_field4=arrow::field("month", arrow::utf8());
+    auto d_field5=arrow::field("year", arrow::int64());
+    auto d_field6=arrow::field("year month num", arrow::int64());
+    auto d_field7=arrow::field("year month", arrow::utf8());
+    auto d_field8=arrow::field("day num in week", arrow::int64());
+    auto d_field9=arrow::field("day num in month", arrow::int64());
+    auto d_field10=arrow::field("day num in year", arrow::int64());
+    auto d_field11=arrow::field("month num in year", arrow::int64());
+    auto d_field12=arrow::field("week num in year", arrow::int64());
+    auto d_field13=arrow::field("selling season", arrow::utf8());
+    auto d_field14=arrow::field("last day in week fl", arrow::int64());
+    auto d_field15=arrow::field("last day in month fl", arrow::int64());
+    auto d_field16=arrow::field("holiday fl", arrow::int64());
+    auto d_field17=arrow::field("weekday fl", arrow::int64());
 
     d_schema=arrow::schema({d_field1,d_field2,d_field3,d_field4,d_field5,
                             d_field6,d_field7,d_field8,d_field9,d_field10,
                             d_field11,d_field12,d_field13,d_field14,d_field15,
                             d_field16,d_field17});
 
-
-
-
-    std::shared_ptr<arrow::Field>p_field1=arrow::field("part key",
-                                                       arrow::int64());
-    std::shared_ptr<arrow::Field>p_field2=arrow::field("name",
-                                                       arrow::utf8());
-    std::shared_ptr<arrow::Field>p_field3=arrow::field("mfgr",
-                                                       arrow::utf8());
-    std::shared_ptr<arrow::Field>p_field4=arrow::field("category",
-                                                       arrow::utf8());
-    std::shared_ptr<arrow::Field>p_field5=arrow::field("brand1",
-                                                       arrow::utf8());
-    std::shared_ptr<arrow::Field>p_field6=arrow::field("color",
-                                                       arrow::utf8());
-    std::shared_ptr<arrow::Field>p_field7=arrow::field("type",
-                                                       arrow::utf8());
-    std::shared_ptr<arrow::Field>p_field8=arrow::field("size",
-                                                       arrow::int64());
-    std::shared_ptr<arrow::Field>p_field9=arrow::field("container",
-                                                       arrow::utf8());
+    auto p_field1=arrow::field("part key", arrow::int64());
+    auto p_field2=arrow::field("name", arrow::utf8());
+    auto p_field3=arrow::field("mfgr", arrow::utf8());
+    auto p_field4=arrow::field("category", arrow::utf8());
+    auto p_field5=arrow::field("brand1", arrow::utf8());
+    auto p_field6=arrow::field("color", arrow::utf8());
+    auto p_field7=arrow::field("type", arrow::utf8());
+    auto p_field8=arrow::field("size", arrow::int64());
+    auto p_field9=arrow::field("container", arrow::utf8());
 
     p_schema=arrow::schema({p_field1,p_field2,p_field3,p_field4,
                             p_field5,
@@ -187,20 +138,28 @@ void read_from_csv() {
 //    std::cout << "lo" << std::endl;
 }
 
+
+
+
+
+
+
 void run_experiment(int sf, int num_trials=1, bool load=false, bool print=false) {
 
     if (load) read_from_csv();
     SSB workload(sf, print);
 
+    skew_column(workload.lo->get_column(4));
+//    std::cout << workload.lo->get_column(4)->ToString() << std::endl;
     std::cout << "sleeping after loading tables..." << std::endl;
-    sleep(10);
+    sleep(0);
     for (int i = 0; i < num_trials; i++) {
         std::cout << "batch start" << std::endl;
 //
 //        workload.q11();
-        workload.q11();
-        workload.q12();
-        workload.q13();
+//        workload.q11();
+//        workload.q12();
+//        workload.q13();
 
 //        workload.q21();
 //        workload.q22();
@@ -215,23 +174,24 @@ void run_experiment(int sf, int num_trials=1, bool load=false, bool print=false)
 //        workload.q42();
 //        workload.q43();
 
-        workload.q21_lip();
-        workload.q22_lip();
-        workload.q23_lip();
+//        workload.q21_lip();
+//        workload.q22_lip();
+//        workload.q23_lip();
 
-        workload.q31_lip();
+//        workload.q31_lip();
         workload.q32_lip();
-        workload.q33_lip();
-        workload.q34_lip();
+//        workload.q33_lip();
+//        workload.q34_lip();
+//
+//        workload.q41_lip();
+//        workload.q42_lip();
+//        workload.q43_lip();
 
-        workload.q41_lip();
-        workload.q42_lip();
-        workload.q43_lip();
     std::cout << "sleeping..." << std::endl;
     sleep(0);
     }
-
 }
+
 
 int main(int argc, char *argv[]) {
 
@@ -246,26 +206,6 @@ int main(int argc, char *argv[]) {
     } else if (argc == 2) {
         run_experiment(std::stoi(argv[1]));
     } else {
-        run_experiment(1, 1, false, 1);
+        run_experiment(1, 1, false, 0);
     }
-
-//    SSB workload(1, true);
-//    SSB workload(1, false);
-//    SSB workload(10, false);
-
-//    std::string input;
-//    std::cout << ">> ";
-//    std::getline(std::cin, input);
-
-//    while (input != "exit") {
-//        auto t1 = high_resolution_clock::now();
-
-
-
-//        auto t2 = high_resolution_clock::now();
-//        std::cout << duration_cast<milliseconds>(t2 - t1).count() << std::endl;
-//
-//        std::cout << ">> ";
-//        std::getline(std::cin, input);
-//    }
 }
