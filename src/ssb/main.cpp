a95e9b5df80932e1d063f6b0e59c89b2b238e633
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-08-07 08:47:36 -0500

    Run full workload

--- src/ssb/main.cpp
+++ src/ssb/main.cpp
@@ -9,12 +9,12 @@ void read_from_csv() {
     std::shared_ptr <Table> lo, c, s, p, d;
     std::shared_ptr <arrow::Schema> lo_schema, c_schema, s_schema, p_schema, d_schema;
 
-    auto field1 = arrow::field("order key", arrow::int64());
+    auto field1 = arrow::field("order key", arrow::uint32());
     auto field2 = arrow::field("line number", arrow::int64());
     auto field3 = arrow::field("cust key", arrow::int64());
     auto field4 = arrow::field("part key", arrow::int64());
     auto field5 = arrow::field("supp key", arrow::int64());
-    auto field6 = arrow::field("order date", arrow::int64());
+    auto field6 = arrow::field("order date", arrow::uint32());
     auto field7 = arrow::field("ord priority", arrow::utf8());
     auto field8 = arrow::field("ship priority", arrow::int64());
     auto field9 = arrow::field("quantity", arrow::uint8());
@@ -97,25 +97,25 @@ void read_from_csv() {
 
     std::shared_ptr<Table> t;
 
-    t = read_from_csv_file("/mydata/SQL-benchmark-data-generator/ssbgen/customer.tbl", c_schema, 20*BLOCK_SIZE);
-    write_to_file("/mydata/SQL-benchmark-data-generator/ssbgen/customer.hsl", *t);
-    std::cout << "c" << std::endl;
-
-    t = read_from_csv_file("/mydata/SQL-benchmark-data-generator/ssbgen/supplier.tbl", s_schema, 20*BLOCK_SIZE);
-    write_to_file("/mydata/SQL-benchmark-data-generator/ssbgen/supplier.hsl", *t);
-    std::cout << "s" << std::endl;
-
-    t = read_from_csv_file("/mydata/SQL-benchmark-data-generator/ssbgen/date.tbl", d_schema, 20*BLOCK_SIZE);
-    write_to_file("/mydata/SQL-benchmark-data-generator/ssbgen/date.hsl", *t);
-    std::cout << "d" << std::endl;
-
-    t = read_from_csv_file("/mydata/SQL-benchmark-data-generator/ssbgen/part.tbl", p_schema, 20*BLOCK_SIZE);
-    write_to_file("/mydata/SQL-benchmark-data-generator/ssbgen/part.hsl", *t);
-    std::cout << "p" << std::endl;
-
-    t = read_from_csv_file("/mydata/SQL-benchmark-data-generator/ssbgen/lineorder.tbl", lo_schema, 20*BLOCK_SIZE);
-    write_to_file("/mydata/SQL-benchmark-data-generator/ssbgen/lineorder.hsl", *t);
-    std::cout << "lo" << std::endl;
+//    t = read_from_csv_file("/mydata/SQL-benchmark-data-generator/ssbgen/customer.tbl", c_schema, 20*BLOCK_SIZE);
+//    write_to_file("/mydata/SQL-benchmark-data-generator/ssbgen/customer.hsl", *t);
+//    std::cout << "c" << std::endl;
+//
+//    t = read_from_csv_file("/mydata/SQL-benchmark-data-generator/ssbgen/supplier.tbl", s_schema, 20*BLOCK_SIZE);
+//    write_to_file("/mydata/SQL-benchmark-data-generator/ssbgen/supplier.hsl", *t);
+//    std::cout << "s" << std::endl;
+//
+//    t = read_from_csv_file("/mydata/SQL-benchmark-data-generator/ssbgen/date.tbl", d_schema, 20*BLOCK_SIZE);
+//    write_to_file("/mydata/SQL-benchmark-data-generator/ssbgen/date.hsl", *t);
+//    std::cout << "d" << std::endl;
+//
+//    t = read_from_csv_file("/mydata/SQL-benchmark-data-generator/ssbgen/part.tbl", p_schema, 20*BLOCK_SIZE);
+//    write_to_file("/mydata/SQL-benchmark-data-generator/ssbgen/part.hsl", *t);
+//    std::cout << "p" << std::endl;
+//
+//    t = read_from_csv_file("/mydata/SQL-benchmark-data-generator/ssbgen/lineorder.tbl", lo_schema, 20*BLOCK_SIZE);
+//    write_to_file("/mydata/SQL-benchmark-data-generator/ssbgen/lineorder.hsl", *t);
+//    std::cout << "lo" << std::endl;
 
 //    t = read_from_csv_file("/Users/corrado/hustle/src/ssb/data/ssb-01/customer.tbl", c_schema, 20*BLOCK_SIZE);
 //    write_to_file("/Users/corrado/hustle/src/ssb/data/ssb-01/customer.hsl", *t);
@@ -148,8 +148,8 @@ void run_experiment(int sf, int num_trials=1, bool load=false, bool print=false)
 
     if (load) read_from_csv();
     SSB workload(sf, print);
-
-    skew_column(workload.lo->get_column(4));
+//    std::cout << workload.lo->get_column(12)->type()->ToString()<<std::endl;
+//    skew_column(workload.lo->get_column(4));
 //    std::cout << workload.lo->get_column(4)->ToString() << std::endl;
     std::cout << "sleeping after loading tables..." << std::endl;
     sleep(0);
@@ -157,9 +157,11 @@ void run_experiment(int sf, int num_trials=1, bool load=false, bool print=false)
         std::cout << "batch start" << std::endl;
 //
 //        workload.q11();
-//        workload.q11();
-//        workload.q12();
-//        workload.q13();
+//        workload.q11_pipelined();
+
+        workload.q11();
+        workload.q12();
+        workload.q13();
 
 //        workload.q21();
 //        workload.q22();
@@ -173,19 +175,19 @@ void run_experiment(int sf, int num_trials=1, bool load=false, bool print=false)
 //        workload.q41();
 //        workload.q42();
 //        workload.q43();
-
-//        workload.q21_lip();
-//        workload.q22_lip();
-//        workload.q23_lip();
-
-//        workload.q31_lip();
-        workload.q32_lip();
-//        workload.q33_lip();
-//        workload.q34_lip();
 //
-//        workload.q41_lip();
-//        workload.q42_lip();
-//        workload.q43_lip();
+        workload.q21_lip();
+        workload.q22_lip();
+        workload.q23_lip();
+
+        workload.q31_lip();
+        workload.q32_lip();
+        workload.q33_lip();
+        workload.q34_lip();
+
+        workload.q41_lip();
+        workload.q42_lip();
+        workload.q43_lip();
 
     std::cout << "sleeping..." << std::endl;
     sleep(0);
@@ -206,6 +208,6 @@ int main(int argc, char *argv[]) {
     } else if (argc == 2) {
         run_experiment(std::stoi(argv[1]));
     } else {
-        run_experiment(1, 1, false, 0);
+        run_experiment(1, 1, 0, 1);
     }
 }
