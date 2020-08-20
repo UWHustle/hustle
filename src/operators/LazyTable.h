4f0771e03d095a51b6965ba0d59df9af4988c37a
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-08-20 15:52:46 -0500

    New constructor includes index_chunks

--- src/operators/LazyTable.h
+++ src/operators/LazyTable.h
@@ -36,11 +36,7 @@ public:
      * @param filter A Boolean ChunkedArray Datum
      * @param indices An INT64 Array Datum
      */
-    LazyTable(
-            std::shared_ptr<Table> table,
-            arrow::Datum filter,
-            arrow::Datum indices
-    );
+    LazyTable(std::shared_ptr<Table> table, arrow::Datum filter, arrow::Datum indices, arrow::Datum index_chunks);
 
     /**
      * Materialize the active rows of one column of the LazyTable. This is
@@ -68,6 +64,7 @@ public:
     std::shared_ptr<Table> table;
     arrow::Datum filter; // filters are ChunkedArrays
     arrow::Datum indices; // indices are Arrays
+    arrow::Datum index_chunks;
     std::vector<std::shared_ptr<arrow::ChunkedArray>> materialized_cols_;
     std::unordered_map<int, std::shared_ptr<arrow::ChunkedArray>> filtered_cols_;
 
@@ -81,6 +78,9 @@ public:
 
     void get_column(Task* ctx, int i, arrow::Datum& out);
 
+    void set_materialized_column(int i, std::shared_ptr<arrow::ChunkedArray> col);
+
+    bool flag_;
 private:
 //    std::vector<std::shared_ptr<arrow::ChunkedArray>> materialized_cols_;
 //    std::vector<bool> materialized_cols_bitmap_;
