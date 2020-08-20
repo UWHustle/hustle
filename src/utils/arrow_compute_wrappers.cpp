90ec4f2569c501994fdab65c609c78c98714c150
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-08-20 15:22:20 -0500

    New apply_indices_internal2 that accepts index_chunks

--- src/utils/arrow_compute_wrappers.cpp
+++ src/utils/arrow_compute_wrappers.cpp
@@ -5,194 +5,6 @@
 
 namespace hustle {
 
-void Context::compare(
-    Task* ctx,
-    const arrow::Datum& left,
-    const arrow::Datum& right,
-    arrow::compute::CompareOperator compare_operator,
-    arrow::Datum* out) {
-
-    clear_data();
-
-    ctx->spawnTask(CreateTaskChain(
-        CreateLambdaTask([this, left, right, compare_operator, &out](Task* internal) {
-            auto chunked_values = left.chunked_array();
-
-            array_vec_.resize(chunked_values->num_chunks());
-
-            int batch_size = chunked_values->num_chunks() / std::thread::hardware_concurrency() / 20;
-            if (batch_size == 0) batch_size = chunked_values->num_chunks();
-            int num_batches = chunked_values->num_chunks() / batch_size + 1; // if num_chunks is a multiple of batch_size, we don't actually want the +1
-            if (num_batches == 0) num_batches = 1;
-
-            for (int batch_i=0; batch_i<num_batches; batch_i++) {
-                internal->spawnLambdaTask([this, batch_i, &out, right, chunked_values, compare_operator, batch_size] {
-                    int base_i = batch_i * batch_size;
-                    for (int i=base_i; i<base_i+batch_size && i<chunked_values->num_chunks(); i++) {
-
-                        arrow::Status status;
-                        arrow::compute::CompareOptions compare_options(compare_operator);
-                        arrow::Datum block_values;
-
-                        status = arrow::compute::Compare(chunked_values->chunk(i), right, compare_options).Value(&block_values);
-                        evaluate_status(status, __FUNCTION__, __LINE__);
-
-                        array_vec_[i] = block_values.make_array();
-                    }
-                });
-            }
-        }),
-        CreateLambdaTask([this, left, right, compare_operator, &out](Task* internal) {
-//            out->value = std::make_shared<arrow::ChunkedArray>(array_vec_);
-            out_.value = std::make_shared<arrow::ChunkedArray>(array_vec_);
-        })
-    ));
-}
-
-arrow::Datum Context::apply_filter_block(
-    const std::shared_ptr<arrow::Array>& values,
-    const std::shared_ptr<arrow::Array>& filter,
-    arrow::ArrayVector& out) {
-
-    arrow::Status status;
-    arrow::Datum block_filter;
-    //                std::cout << "apply filter" << std::endl;
-    //                std::cout << chunked_values->length() << " " << chunked_filter->length() << std::endl;
-    //                std::cout << chunked_values->num_chunks() << " " << chunked_filter->num_chunks()<< std::endl;
-
-//    arrow::Datum filter_indices;
-//    status = arrow::compute::internal::GetTakeIndices(*filter->data(), arrow::compute::FilterOptions::EMIT_NULL).Value(&filter_indices);
-//    evaluate_status(status, __PRETTY_FUNCTION__, __LINE__);
-//    status = arrow::compute::Take(values, filter_indices).Value(&block_filter);
-//    evaluate_status(status, __PRETTY_FUNCTION__, __LINE__);
-
-    status = arrow::compute::Filter(values, filter).Value(&block_filter);
-    evaluate_status(status, __PRETTY_FUNCTION__, __LINE__);
-    return block_filter.make_array();
-}
-
-void Context::apply_filter_internal(
-    Task* ctx,
-    const arrow::Datum& values,
-    const arrow::Datum& filter,
-    arrow::ArrayVector& out) {
-
-    ctx->spawnTask(CreateTaskChain(
-        CreateLambdaTask([this, values, filter, &out](Task* internal) {
-            const auto& chunked_values = values.chunked_array();
-            const auto& chunked_filter = filter.chunked_array();
-
-            out.resize(chunked_values->num_chunks());
-
-            int batch_size = chunked_values->num_chunks() /  std::thread::hardware_concurrency() / 20;
-            if (batch_size == 0) batch_size = chunked_values->num_chunks();
-            int num_batches = chunked_values->num_chunks() / batch_size + 1; // if num_chunks is a multiple of batch_size, we don't actually want the +1
-            if (num_batches == 0) num_batches = 1;
-
-            for (int batch_i=0; batch_i<num_batches; batch_i++) {
-                internal->spawnLambdaTask([this, batch_i, &out, chunked_filter, chunked_values, batch_size] {
-                    int base_i = batch_i * batch_size;
-                    for (int i=base_i; i<base_i+batch_size && i<chunked_values->num_chunks(); i++) {
-                        auto block_filter = apply_filter_block(chunked_values->chunk(i), chunked_filter->chunk(i), out);
-                        out[i] = block_filter.make_array();
-                    }
-                });
-            }
-        })
-    ));
-}
-
-void Context::apply_filter(
-    Task* ctx,
-    const arrow::Datum& values,
-    const arrow::Datum& filter,
-    arrow::Datum& out) {
-
-    ctx->spawnTask(CreateTaskChain(
-        CreateLambdaTask([this, values, filter, &out](Task* internal) {
-            clear_data();
-
-            arrow::Status status;
-
-            switch(values.kind()) {
-                case arrow::Datum::NONE:
-                    break;
-                case arrow::Datum::ARRAY: {
-                    status = arrow::compute::Filter(values, filter.make_array()).Value(&out);
-                    break;
-                }
-                case arrow::Datum::CHUNKED_ARRAY: {
-
-            apply_filter_internal(internal, values, filter, array_vec_);
-                    break;
-                }
-                default: {
-                    std::cerr << "Value kind not supported" << std::endl;
-                }
-            }
-
-            evaluate_status(status, __PRETTY_FUNCTION__, __LINE__);
-        }),
-        CreateLambdaTask([this, values, filter, &out](Task* internal) {
-            if (values.kind() == arrow::Datum::CHUNKED_ARRAY) {
-                out.value = std::make_shared<arrow::ChunkedArray>(array_vec_);
-            }
-        })
-    ));
-
-}
-
-void apply_filter(
-    const arrow::Datum& values,
-    const arrow::Datum& filter,
-    arrow::Datum* out) {
-
-    arrow::Status status;
-    switch(values.kind()) {
-        case arrow::Datum::NONE:
-            break;
-        case arrow::Datum::ARRAY: {
-            status = arrow::compute::Filter(values,filter.make_array()).Value(out);
-            break;
-        }
-        case arrow::Datum::CHUNKED_ARRAY: {
-            status = arrow::compute::Filter(values,filter.chunked_array()).Value(out);
-            break;
-        }
-        default: {
-            std::cerr << "Value kind not supported" << std::endl;
-        }
-    }
-    evaluate_status(status, __PRETTY_FUNCTION__, __LINE__);
-}
-
-void apply_indices(
-    const arrow::Datum& values,
-    const arrow::Datum& indices,
-    arrow::Datum* out) {
-
-    arrow::Status status;
-    arrow::compute::TakeOptions take_options;
-
-    switch(values.kind()) {
-        case arrow::Datum::NONE:
-            break;
-        case arrow::Datum::ARRAY:
-            status = arrow::compute::Take(values.make_array(), indices.make_array()).Value(out);
-            break;
-        case arrow::Datum::CHUNKED_ARRAY: {
-            std::shared_ptr<arrow::ChunkedArray> temp_chunked_array;
-            status = arrow::compute::Take(
-                *values.chunked_array(), *indices.make_array()).Value(&temp_chunked_array);
-            out->value = temp_chunked_array;
-            break;
-        }
-        default:
-            std::cerr << "Value kind not supported" << std::endl;
-    }
-    evaluate_status(status, __PRETTY_FUNCTION__, __LINE__);
-}
-
 Context::Context() {
     slice_length_ = 30000;
 }
@@ -263,6 +75,57 @@ void Context::apply_indices_internal(
         // Find the chunk to which index belongs
         auto chunk_j = (std::upper_bound(offsets_data, offsets_data_end, index) - offsets_data) - 1;
         out[i] = values_data_vec[chunk_j][index - offsets_data[chunk_j]];
+    }
+
+    array_vec_[slice_i] = arrow::MakeArray(out_data);
+
+}
+
+template<typename T>
+void Context::apply_indices_internal2(
+    const std::shared_ptr<arrow::ChunkedArray>& chunked_values,
+    const T ** values_data_vec,
+    const std::shared_ptr<arrow::Array>& indices_array,
+    const std::shared_ptr<arrow::Array>& index_chunks,
+    int slice_i) {
+
+    int num_slices = indices_array->length()/slice_length_ + 1;
+//    int num_slices = chunked_values->num_chunks();
+//    slice_length_ = chunked_values->chunk(slice_i)->length();
+
+    std::shared_ptr<arrow::Array> sliced_indices;
+    std::shared_ptr<arrow::Array> sliced_index_chunks;
+
+    if (slice_i == num_slices-1) {
+        sliced_indices = indices_array->Slice(slice_i*slice_length_, indices_array->length() - (slice_i-1)*slice_length_);
+        sliced_index_chunks = index_chunks->Slice(slice_i*slice_length_, indices_array->length() - (slice_i-1)*slice_length_);
+    }
+    else {
+        sliced_indices = indices_array->Slice(slice_i*slice_length_, slice_length_);
+        sliced_index_chunks = index_chunks->Slice(slice_i*slice_length_, slice_length_);
+    }
+
+    if (sliced_indices->length() == 0) {
+        array_vec_[slice_i] = arrow::MakeArrayOfNull(chunked_values->type(), 0, arrow::default_memory_pool()).ValueOrDie();
+        return;
+    }
+
+    auto index_chunks_data = index_chunks->data()->GetValues<uint16_t>(1, 0);
+    auto sliced_indices_data = sliced_indices->data()->GetValues<uint32_t>(1);
+
+    std::shared_ptr<arrow::Buffer> out_buffer;
+    auto status = arrow::AllocateBuffer(sliced_indices->length()*sizeof(int64_t)).Value(&out_buffer);
+    evaluate_status(status, __PRETTY_FUNCTION__, __LINE__);
+
+    //@TODO: this forces the output to be INT64!
+    auto out_data = arrow::ArrayData::Make(arrow::int64(), sliced_indices->length(), {nullptr, out_buffer});
+    auto out = out_data->GetMutableValues<int64_t>(1);
+
+    for (uint32_t i=0; i<sliced_indices->length(); ++i) {
+        auto index = sliced_indices_data[i];
+        auto chunk_j = index_chunks_data[i];
+        std::cout << chunk_j << " " << index << values_data_vec[chunk_j][index] << std::endl;
+        out[i] = values_data_vec[chunk_j][index];
     }
 
     array_vec_[slice_i] = arrow::MakeArray(out_data);
@@ -364,12 +227,12 @@ void Context::apply_indices(
     Task* ctx,
     const arrow::Datum values,
     const arrow::Datum indices,
-    bool has_sorted_indices,
+    const arrow::Datum index_chunks,
     arrow::Datum& out) {
 
     clear_data();
     ctx->spawnTask(CreateTaskChain(
-        CreateLambdaTask([this, values, indices, has_sorted_indices, &out](Task* internal)  {
+        CreateLambdaTask([this, values, indices, index_chunks, &out](Task* internal)  {
 
             arrow::Status status;
             std::shared_ptr<arrow::ChunkedArray> chunked_values;
@@ -394,7 +257,6 @@ void Context::apply_indices(
             else {
                 indices_array = indices.make_array();
             }
-            auto indices_data = indices_array->data()->GetMutableValues<int32_t>(1, 0);
 
             int num_chunks = chunked_values->num_chunks();
             std::vector<unsigned int> chunk_row_offsets;
@@ -416,8 +278,10 @@ void Context::apply_indices(
 
             array_vec_.resize(num_slices);
 
+            auto has_index_chunks = index_chunks.kind() != arrow::Datum::NONE;
+
             for (int i=0; i<num_slices; i++) {
-                internal->spawnLambdaTask([this, indices_array, chunked_values, offsets, i]{
+                internal->spawnLambdaTask([this, indices_array, chunked_values, has_index_chunks, offsets, index_chunks, i]{
 //                    apply_indices_internal(chunked_values, indices_array, offsets, i);
                     switch(chunked_values->type()->id()) {
                         case arrow::Type::INT64: {
@@ -425,7 +289,11 @@ void Context::apply_indices(
                             for (int i=0; i<chunked_values->num_chunks(); ++i) {
                                 values_data_vec[i] = chunked_values->chunk(i)->data()->GetValues<int64_t>(1);
                             }
-                            apply_indices_internal<int64_t>(chunked_values, values_data_vec.data(), indices_array, offsets, i);
+                            if (has_index_chunks) {
+                                apply_indices_internal2<int64_t>(chunked_values, values_data_vec.data(), indices_array, index_chunks.make_array(), i);
+                            } else {
+                                apply_indices_internal<int64_t>(chunked_values, values_data_vec.data(), indices_array, offsets, i);
+                            }
                             break;
                         }
                         case arrow::Type::UINT32: {
@@ -433,7 +301,11 @@ void Context::apply_indices(
                             for (int i=0; i<chunked_values->num_chunks(); ++i) {
                                 values_data_vec[i] = chunked_values->chunk(i)->data()->GetValues<uint32_t>(1);
                             }
-                            apply_indices_internal<uint32_t>(chunked_values, values_data_vec.data(), indices_array, offsets, i);
+                            if (has_index_chunks) {
+                                apply_indices_internal2<uint32_t>(chunked_values, values_data_vec.data(), indices_array, index_chunks.make_array(), i);
+                            } else {
+                                apply_indices_internal<uint32_t>(chunked_values, values_data_vec.data(), indices_array, offsets, i);
+                            }
                             break;
                         }
                         case arrow::Type::STRING: {
@@ -445,7 +317,7 @@ void Context::apply_indices(
                 });
             }
         }),
-        CreateLambdaTask([this, values, indices, has_sorted_indices, &out](Task* internal)  {
+        CreateLambdaTask([this, values, indices, &out](Task* internal)  {
 
             arrow::Status status;
             std::shared_ptr<arrow::Array> arr;
@@ -481,15 +353,6 @@ void sort_to_indices(const arrow::Datum& values, arrow::Datum* out) {
     }
     out->value = temp->data();
 }
-
-void sort_datum(const arrow::Datum& values, arrow::Datum* out) {
-
-    arrow::Datum sorted_indices;
-
-    sort_to_indices(values, &sorted_indices);
-    apply_indices(values, sorted_indices, out);
-}
-
 
 void compare(
     const arrow::Datum& left,
