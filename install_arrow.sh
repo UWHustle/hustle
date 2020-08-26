1a5f75bff2fce10263b3cf89d63e99d54ba40291
Nicholas Corrado <corrado@Nicholass-MacBook-Pro.local>
2020-08-26 04:52:15 -0500

    Clone Arrow 1.0.1. Set additional compiler flag to ensure that utf8proc is found.

--- install_arrow.sh
+++ install_arrow.sh
@@ -2,14 +2,15 @@ if [ ! -d "arrow" ]
 then
   git clone https://github.com/apache/arrow.git
   cd arrow
-  git checkout 4d99ec48a0f36531124a368afe7818b56e25dfb3
+  git checkout apache-arrow-1.0.1
   cd cpp
   mkdir release
   cd release
   if [[ `uname` == "Darwin" ]]; then
-    cmake -DARROW_COMPUTE=ON -DARROW_CSV=ON ..
+    # -DARROW_DEPENDENCY_SOURCE=AUTO: try to build dependencies from source if they are not found on the machine.
+    cmake -DARROW_COMPUTE=ON -DARROW_DEPENDENCY_SOURCE=AUTO ...
   elif [[ `uname` == "Linux" ]]; then
-    ../../../cmake-3.15.5/bin/cmake -DARROW_COMPUTE=ON -DARROW_CSV=ON ..
+    ../../../cmake-3.15.5/bin/cmake -DARROW_COMPUTE=ON -DARROW_DEPENDENCY_SOURCE=AUTO ..
   fi
   sudo make install -j4
 else
