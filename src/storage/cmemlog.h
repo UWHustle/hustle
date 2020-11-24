0ffd9aa19e16de83e7b8f47d8971f03a5da2c81e
SURYADEV <srsuryadev@gmail.com>
2020-11-23 20:56:55 -0600

    Handling error cases and returning status from Memlog

--- src/storage/cmemlog.h
+++ src/storage/cmemlog.h
@@ -19,6 +19,8 @@
 #include <stdio.h>
 
 #define MEMLOG_INIT_SIZE 100
+#define MEMLOG_OK 0
+#define MEMLOG_ERROR 1
 
 struct DBRecordList {
     struct DBRecord *head;
@@ -32,6 +34,7 @@ struct DBRecord {
   struct DBRecord *next_record;
 };
 
+typedef int Status;
 typedef struct DBRecord DBRecord;
 typedef struct DBRecordList DBRecordList;
 
@@ -40,7 +43,10 @@ typedef struct {
   int total_size;
 } HustleMemLog;
 
-void hustle_memlog_initialize(HustleMemLog **mem_log, int initial_size) {
+Status hustle_memlog_initialize(HustleMemLog **mem_log, int initial_size) {
+  if (initial_size <= 0) {
+      return MEMLOG_ERROR;
+  }
   *mem_log = (HustleMemLog*)malloc(sizeof(HustleMemLog)); 
   (*mem_log)->record_list = (DBRecordList *)malloc(initial_size * sizeof(DBRecordList));
   (*mem_log)->total_size = initial_size;
@@ -51,10 +57,14 @@ void hustle_memlog_initialize(HustleMemLog **mem_log, int initial_size) {
     (*mem_log)->record_list[table_index].curr_size = 0;
     table_index++;
   }
+  return MEMLOG_OK;
 }
 
 
 DBRecord* hustle_memlog_create_record(const void *data, int nData) {
+    if (data == NULL) {
+      return NULL;
+    }
     DBRecord* record = (DBRecord*) malloc(sizeof(DBRecord));
     record->data = data;
     record->nData = nData;
@@ -62,7 +72,10 @@ DBRecord* hustle_memlog_create_record(const void *data, int nData) {
     return record;
 }
 
-void hustle_memlog_insert_record(HustleMemLog *mem_log, DBRecord *record, int table_id) {
+Status hustle_memlog_insert_record(HustleMemLog *mem_log, DBRecord *record, int table_id) {
+  if (mem_log == NULL || record == NULL) {
+    return MEMLOG_ERROR;
+  }
   if (table_id >= mem_log->total_size) {
     int old_table_list_size = mem_log->total_size;
     mem_log->total_size *= 2;
@@ -86,9 +99,20 @@ void hustle_memlog_insert_record(HustleMemLog *mem_log, DBRecord *record, int ta
       mem_log->record_list[table_id].head = record;
   }
   mem_log->record_list[table_id].curr_size += 1; 
+  return MEMLOG_OK;
 }
 
-void hustle_memlog_update_db(HustleMemLog *mem_log) {
+DBRecordList* hustle_memlog_get_records(HustleMemLog *mem_log, int table_id) {
+  if (mem_log == NULL) {
+    return NULL;
+  }
+  return &mem_log->record_list[table_id];
+}
+
+Status hustle_memlog_update_db(HustleMemLog *mem_log) {
+  if (mem_log == NULL) {
+    return MEMLOG_ERROR;
+  }
   int table_index = 0;
   struct DBRecord *tmp_record;
   while (table_index < mem_log->total_size) {
@@ -101,10 +125,14 @@ void hustle_memlog_update_db(HustleMemLog *mem_log) {
     }
     table_index++;
   }
+  return MEMLOG_OK;
 }
 
 
-void hustle_memlog_update_db_free(HustleMemLog *mem_log) {
+Status hustle_memlog_update_db_free(HustleMemLog *mem_log) {
+  if (mem_log == NULL) {
+    return MEMLOG_ERROR;
+  }
   int table_index = 0;
   struct DBRecord *tmp_record;
   while (table_index < mem_log->total_size) {
@@ -122,10 +150,13 @@ void hustle_memlog_update_db_free(HustleMemLog *mem_log) {
     mem_log->record_list[table_index].curr_size = 0;
     table_index++;
   }
-  
+  return MEMLOG_OK;
 }
 
-void hustle_memlog_clear(HustleMemLog *mem_log) {
+Status hustle_memlog_clear(HustleMemLog *mem_log) {
+  if (mem_log == NULL) {
+    return MEMLOG_ERROR;
+  }
   struct DBRecord *tmp_record;
   int table_index = 0;
   while (table_index < mem_log->total_size) {
@@ -140,11 +171,16 @@ void hustle_memlog_clear(HustleMemLog *mem_log) {
     mem_log->record_list[table_index].curr_size = 0;
     table_index++;
   }
+  return MEMLOG_OK;
 }
 
-void hustle_memlog_free(HustleMemLog *mem_log) {
+Status hustle_memlog_free(HustleMemLog *mem_log) {
+  if (mem_log == NULL) {
+    return MEMLOG_ERROR;
+  }
   hustle_memlog_clear(mem_log);
 
   free(mem_log->record_list);
   free(mem_log);
+  return MEMLOG_OK;
 }
