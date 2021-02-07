63c0056f9bc8de09b8168250614da9f61e0d26ad
SURYADEV <srsuryadev@gmail.com>
2021-02-06 22:15:07 -0600

    Adding expression evaluation files

---
+++ src/operators/expression.cc
@@ -0,0 +1,176 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include "operators/expression.h"
+
+#include <algorithm>
+#include <cassert>
+
+#include "scheduler/threading/synchronization_lock.h"
+
+namespace hustle::operators {
+
+Expression::Expression(std::shared_ptr<OperatorResult> prev_op_output,
+                       std::shared_ptr<ExprReference> expr)
+    : expr_(expr), prev_op_output_(prev_op_output) {
+  exp_num_chunks_ = 0;
+}
+
+void Expression::Initialize(hustle::Task* ctx) {
+  if (expr_ == nullptr) {
+    throw std::runtime_error("Expression is expected here.");
+  }
+  this->ConvertPostfix(ctx, expr_);
+}
+
+void Expression::ConvertPostfix(hustle::Task* ctx,
+                                std::shared_ptr<ExprReference> expr) {
+  if (expr == nullptr) {
+    return;
+  }
+  this->ConvertPostfix(ctx, expr->left_expr);
+  this->ConvertPostfix(ctx, expr->right_expr);
+
+  std::shared_ptr<arrow::ChunkedArray> column = nullptr;
+  if (expr->op == TK_COLUMN || expr->op == TK_AGG_COLUMN) {
+    arrow::Datum col;
+    prev_op_output_->get_table(expr->column_ref->table)
+        .get_column_by_name(ctx, expr->column_ref->col_name, col);
+    column = col.chunked_array();
+    exp_num_chunks_ = std::max(exp_num_chunks_, column->num_chunks());
+  }
+  postfix_expr_.push_back({expr->op, column});
+}
+
+template <typename ArrayType, typename ArrayPrimitiveType>
+arrow::Datum Expression::ExecuteBlock(int op,
+                                      std::shared_ptr<arrow::Array> result,
+                                      std::shared_ptr<arrow::Array> left_col,
+                                      std::shared_ptr<arrow::Array> right_col) {
+  /*std::shared_ptr<ArrayType> result,
+  std::shared_ptr<ArrayType> left_col,
+  std::shared_ptr<ArrayType> right_col) {*/
+  int chunk_length = std::max(left_col->length(), right_col->length());
+  auto left_col_data =
+      left_col->data()->template GetValues<ArrayPrimitiveType>(1, 0);
+  auto right_col_data =
+      right_col->data()->template GetValues<ArrayPrimitiveType>(1, 0);
+  std::shared_ptr<ArrayType> result_arr =
+      std::static_pointer_cast<ArrayType>(result);
+
+  auto result_data =
+      result->data()->template GetMutableValues<ArrayPrimitiveType>(1, 0);
+  switch (op) {
+    case TK_PLUS: {
+      for (std::size_t row = 0; row < chunk_length; row++) {
+        result_data[row] = left_col_data[row] + right_col_data[row];
+      }
+      break;
+    }
+    case TK_MINUS: {
+      for (std::size_t row = 0; row < chunk_length; row++) {
+        result_data[row] = left_col_data[row] - right_col_data[row];
+      }
+      break;
+    }
+    case TK_SLASH: {
+      for (std::size_t row = 0; row < chunk_length; row++) {
+        result_data[row] = left_col_data[row] / right_col_data[row];
+      }
+      break;
+    }
+    case TK_STAR: {
+      for (std::size_t row = 0; row < chunk_length; row++) {
+        result_data[row] = left_col_data[row] * right_col_data[row];
+      }
+      break;
+    }
+  }
+  return result;
+}
+
+template <typename ArrayType, typename ArrayPrimitiveType>
+arrow::Datum Expression::ExecuteBlock(bool is_result,
+                                      const arrow::Scalar& scalar, int op,
+                                      std::shared_ptr<arrow::Array> left_col,
+                                      std::shared_ptr<arrow::Array> right_col) {
+  // std::shared_ptr<ArrayType> left_col,
+  // std::shared_ptr<ArrayType> right_col) {
+  if (is_result) {
+    std::shared_ptr<ArrayType> result = std::static_pointer_cast<ArrayType>(
+        arrow::MakeArrayFromScalar(scalar, left_col->length()).ValueOrDie());
+    return this->ExecuteBlock<ArrayType, ArrayPrimitiveType>(
+        op, result, left_col, right_col);
+  } else {
+    return this->ExecuteBlock<ArrayType, ArrayPrimitiveType>(
+        op, left_col, left_col, right_col);
+  }
+}
+
+arrow::Datum Expression::Evaluate(hustle::Task* ctx, int chunk_id) {
+  std::stack<OpElem> eval_stack;
+  arrow::Datum result;
+
+  assert((postfix_expr_.size() != 0) && "Initialize() the expression object");
+  for (int i = 0; i < postfix_expr_.size(); i++) {
+    ExprElem expr_item = postfix_expr_[i];
+    if (expr_item.op == TK_COLUMN || expr_item.op == TK_AGG_COLUMN) {
+      eval_stack.push({expr_item.op, false, expr_item.col->chunk(chunk_id)});
+    } else {
+      OpElem left_op = eval_stack.top();
+      eval_stack.pop();
+      OpElem right_op = eval_stack.top();
+      eval_stack.pop();
+
+      bool is_result = false;
+      if (!right_op.is_result && !right_op.is_result) {
+        is_result = true;
+      }
+      int op = expr_item.op;
+      auto l_chunk = left_op.chunk.make_array();
+      auto r_chunk = right_op.chunk.make_array();
+
+      if (l_chunk->type() != r_chunk->type()) {
+        throw std::runtime_error(
+            "Implicit type conversion in"
+            "expression evaluation currently not supported.");
+      }
+      switch (l_chunk->type()->id()) {
+        case arrow::Type::INT64: {
+          result =
+              this->ExecuteBlock<arrow::Int64Array, int64_t>(
+                      is_result, arrow::Int64Scalar(0), op, l_chunk, r_chunk)
+                  .make_array();
+          break;
+        }
+        default: {
+          throw std::runtime_error(
+              "Expression Evaluation"
+              "not supported for this type.");
+        }
+      }
+      std::cout << "Expression push result" << std::endl;
+      eval_stack.push({TK_AGG_COLUMN, true, arrow::Datum(result)});
+    }
+  }
+  if (!eval_stack.empty()) {
+    return arrow::Datum(eval_stack.top().chunk);
+  }
+  throw std::runtime_error("Invalid expression");
+}
+
+}  // namespace hustle::operators
--- src/operators/expression.h
+++ src/operators/expression.h
@@ -30,6 +30,7 @@
 #include "operators/utils/operator_result.h"
 
 #define TK_COLUMN 164
+#define TK_AGG_COLUMN 166
 #define TK_PLUS 105
 #define TK_MINUS 106
 #define TK_STAR 107
@@ -40,148 +41,47 @@ namespace operators {
 
 struct ExprElem {
   int op;
-  bool is_result_col;
   std::shared_ptr<arrow::ChunkedArray> col;
 };
+
+struct OpElem {
+  int op;
+  bool is_result;
+  arrow::Datum chunk;
+};
+
+typedef struct ExprElem ExprElem;
+typedef struct OpElem OpElem;
 
 class Expression {
  private:
   std::shared_ptr<ExprReference> expr_;
-  std::vector<std::shared_ptr<arrow::Array>> result_vector_;
+  std::vector<ExprElem> postfix_expr_;
+  std::shared_ptr<OperatorResult> prev_op_output_;
 
-  void ConvertPostFix(std::shared_ptr<ExprReference> expr,
-                      std::vector<ExprElem>& postfix_expr) {
-    if (expr == nullptr) {
-      return;
-    }
-    this->ConvertPostFix(expr->left_expr, postfix_expr);
-    this->ConvertPostFix(expr->right_expr, postfix_expr);
+  int32_t exp_num_chunks_;
 
-    std::shared_ptr<arrow::ChunkedArray> column = nullptr;
-    if (expr->op == TK_COLUMN) {
-      column = expr->column_ref->table->get_column_by_name(
-          expr->column_ref->col_name);
-    }
-    postfix_expr.push_back({expr->op, false, column});
-  }
+  void ConvertPostfix(hustle::Task* ctx, std::shared_ptr<ExprReference> expr);
 
  public:
-  Expression(std::shared_ptr<ExprReference> expr) : expr_(expr) {}
+  Expression(std::shared_ptr<OperatorResult> prev_op_output,
+             std::shared_ptr<ExprReference> expr);
 
+  void Initialize(hustle::Task* ctx);
+
+  template <typename ArrayType, typename ArrayPrimitiveType>
   arrow::Datum ExecuteBlock(int op, std::shared_ptr<arrow::Array> result,
-                            std::shared_ptr<arrow::Array> col_1,
-                            std::shared_ptr<arrow::Array> col_2) {
-    int chunk_length = col_1->length();
-    auto col_data_1 = col_1->data()->GetValues<uint64_t>(1, 0);
-    auto col_data_2 = col_2->data()->GetValues<uint64_t>(1, 0);
-    auto result_arr = std::static_pointer_cast<arrow::Int64Array>(result);
+                            std::shared_ptr<arrow::Array> left_col,
+                            std::shared_ptr<arrow::Array> right_col);
 
-    auto result_data = result_arr->values()->mutable_data();
-    switch (op) {
-      case TK_PLUS: {
-        for (std::size_t row = 0; row < chunk_length; row++) {
-          result_data[row] = col_data_1[row] + col_data_2[row];
-        }
-        break;
-      }
-      case TK_MINUS: {
-        for (std::size_t row = 0; row < chunk_length; row++) {
-          result_data[row] = col_data_1[row] - col_data_2[row];
-        }
-        break;
-      }
-      case TK_SLASH: {
-        for (std::size_t row = 0; row < chunk_length; row++) {
-          result_data[row] = col_data_1[row] / col_data_2[row];
-        }
-        break;
-      }
-      case TK_STAR: {
-        for (std::size_t row = 0; row < chunk_length; row++) {
-          result_data[row] = col_data_1[row] * col_data_2[row];
-        }
-        break;
-      }
-    }
-    return result;
-  }
+  template <typename ArrayType, typename ArrayPrimitiveType>
+  arrow::Datum ExecuteBlock(bool is_result, const arrow::Scalar& scalar, int op,
+                            std::shared_ptr<arrow::Array> left_col,
+                            std::shared_ptr<arrow::Array> right_col);
 
-  void EvaluateExpression(Task* ctx, arrow::Datum& out) {
-    if (expr_ == nullptr) {
-      return;
-    }
-    std::vector<ExprElem> postfix_expr(10);
-    // result_vector_.resize(col_1->num_chunks());
+  arrow::Datum Evaluate(hustle::Task* ctx, int chunk_id);
 
-    this->ConvertPostFix(expr_, postfix_expr);
-
-    // result_vector.push_back(nullptr);
-
-    std::stack<ExprElem> eval_stack;
-    for (ExprElem expr_item : postfix_expr) {
-      if (expr_item.op == TK_COLUMN) {
-        if (result_vector_.size() < expr_item.col->num_chunks()) {
-          result_vector_.resize(expr_item.col->num_chunks());
-        }
-        eval_stack.push(expr_item);
-      } else {
-        ExprElem expr_item1 = eval_stack.top();
-        eval_stack.pop();
-        ExprElem expr_item2 = eval_stack.top();
-        eval_stack.pop();
-        bool is_result = false;
-
-        auto col_1 = expr_item1.col;
-        auto col_2 = expr_item2.col;
-
-        if (!expr_item1.is_result_col && !expr_item2.is_result_col) {
-          is_result = true;
-        }
-
-        int batch_size =
-            col_1->num_chunks() / std::thread::hardware_concurrency() / 2;
-        if (batch_size == 0) batch_size = col_1->num_chunks();
-        int num_batches = (col_1->num_chunks() / batch_size) + 1;
-        int op = expr_item.op;
-        for (std::size_t batch_idx = 0; batch_idx < num_batches; batch_idx++) {
-          ctx->spawnLambdaTask(
-              [this, batch_idx, batch_size, op, col_1, col_2, is_result] {
-                int base_idx = batch_idx * batch_size;
-                int num_chunks = col_1->num_chunks();
-                int chunk_limit = base_idx + batch_size;
-                for (size_t chunk_idx = base_idx;
-                     (chunk_idx < chunk_limit && chunk_idx < num_chunks);
-                     ++chunk_idx) {
-                  auto chunk_1 = col_1->chunk(chunk_idx);
-                  auto chunk_2 = col_2->chunk(chunk_idx);
-                  if (is_result) {
-                    std::shared_ptr<arrow::Int64Array> result =
-                        std::static_pointer_cast<arrow::Int64Array>(
-                            arrow::MakeArrayFromScalar(arrow::Int64Scalar(0),
-                                                       num_chunks)
-                                .ValueOrDie());
-                    result_vector_[chunk_idx] =
-                        this->ExecuteBlock(op, result, chunk_1, chunk_2)
-                            .make_array();
-                  } else {
-                    result_vector_[chunk_idx] =
-                        this->ExecuteBlock(op, chunk_1, chunk_1, chunk_2)
-                            .make_array();
-                  }
-                }
-              });
-        }
-        ExprElem result_expr_item = {
-            TK_COLUMN, false,
-            std::make_shared<arrow::ChunkedArray>(result_vector_)};
-        eval_stack.push(result_expr_item);
-      }
-    }
-    if (!eval_stack.empty()) {
-      ExprElem result_expr_item = eval_stack.top();
-      out = std::make_shared<arrow::ChunkedArray>(result_vector_);
-    }
-  }
+  inline int32_t num_chunks() { return exp_num_chunks_; }
 };
 }  // namespace operators
 }  // namespace hustle
