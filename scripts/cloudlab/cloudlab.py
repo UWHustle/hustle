c42b67692b156ff200ea9e58a8e87876f84a656b
mprammer2 <prammer@wisc.edu>
2021-02-28 11:31:20 -0600

    default experiment and improved logging

--- scripts/cloudlab/cloudlab.py
+++ scripts/cloudlab/cloudlab.py
@@ -3,17 +3,23 @@ import sys
 import subprocess
 import datetime
 import csv
+import os
 
 time_format = "%Y-%m-%d %H:%M:%S"
 
 REPO_DIR = "/mydata/repo/"
 RESULT_DIR = "/mydata/results/"
 HUSTLE_BUILD_DIR = "build_release/"
-SSB_BENCHMARK_PATH = REPO_DIR + HUSTLE_BUILD_DIR + "src/benchmark/hustle_src_benchmark_main"
+SSB_BENCHMARK_DIR = REPO_DIR + HUSTLE_BUILD_DIR + "src/benchmark/"
+SSB_BENCHMARK_PATH = SSB_BENCHMARK_DIR + "hustle_src_benchmark_main"
+SSB_BENCHMARK_DEFAULT_ARGS = ["ssb", "hash-aggregate"]
+
+os.chdir(SSB_BENCHMARK_DIR)
 
 
-def parse_bench_out(experiment_num, str_output):
+def parse_bench_out(a_experiment_num, str_output):
     out = []
+    str_experiment_num = str(a_experiment_num)  # allow inputs that convert to string
     str_arr = str_output.split("\\n")
     # sample line:
     # query11      41149293 ns      2607978 ns          100
@@ -47,16 +53,26 @@ if __name__ == '__main__':
         args["experiment_4_flags"],
         args["experiment_5_flags"],
     ]
-    experiment_results = []
     print(datetime.datetime.now().strftime(time_format) + " | Starting Experiments...")
+    experiment_results = []
+    print(datetime.datetime.now().strftime(time_format) + " | Starting default experiment...")
+    default_command = [SSB_BENCHMARK_PATH]
+    default_command.extend(SSB_BENCHMARK_DEFAULT_ARGS)
+    process = subprocess.Popen(default_command, shell=True)
+    process.wait()
+    proc_out, proc_err = process.communicate()
+    experiment_results.append(parse_bench_out("default", str(proc_out, 'utf-8')))
+    print(datetime.datetime.now().strftime(time_format) + " | Starting numbered experiments...")
     for experiment_flags, experiment_num in zip(experiments, [1, 2, 3, 4, 5]):
         if experiment_flags != "":
-            command = [SSB_BENCHMARK_PATH]
             flag_list = experiment_flags.split(" ")
+            command = [SSB_BENCHMARK_PATH]
+            command.extend(SSB_BENCHMARK_DEFAULT_ARGS)
             command.extend(flag_list)
             print(
                 datetime.datetime.now().strftime(time_format) + " | Starting Experiment #" + str(experiment_num) + "..."
             )
+            print(datetime.datetime.now().strftime(time_format) + " | Experiment Flags: " + experiment_flags)
             process = subprocess.Popen(command, shell=True)
             process.wait()
             proc_out, proc_err = process.communicate()
