9a44f041dfe80968d4e8154dc5161f38112b249f
mprammer2 <prammer@wisc.edu>
2021-03-05 19:36:29 -0600

    baseline cloudlab integration

--- profile.py
+++ profile.py
@@ -18,9 +18,9 @@
 """Hustle Cloudlab Repeatable Experiment Profile
 
 Default behavior:
-By default, this uses a c220g5 with 100GB and runs experiments at scale factor 1.
-A baseline experiment is always run before numbered experiments.
-Numbered experiments will not be run unless provided with one or more flags to use.
+By default, this uses a c220g5 with 100GB of storage and runs experiments at scale factor 1.
+Numbered experiments will not be run unless provided with one or more arguments to use.
+A common argument can be provided that will precede all per-experiment arguments.
 Storage size may need to be increased for larger scale factors.
 
 Instructions:
@@ -32,15 +32,23 @@ import geni.rspec.pg as pg
 
 import json
 
+try:
+    import urllib.parse as url_parser
+except ImportError:
+    import urllib as url_parser
+
 pc = portal.Context()
 pc.defineParameter("hardware", "Hardware (Default: c220g5)", portal.ParameterType.STRING, "c220g5")
 pc.defineParameter("storage", "Storage Size (Default: 100GB)", portal.ParameterType.STRING, "100GB")
 pc.defineParameter("scale_factor", "SSB Scale Factor (Default: 1)", portal.ParameterType.INTEGER, 1)
-pc.defineParameter("experiment_1_flags", "Experiment 1 Flags (Default: \"\"", portal.ParameterType.STRING, "")
-pc.defineParameter("experiment_2_flags", "Experiment 2 Flags (Default: \"\"", portal.ParameterType.STRING, "")
-pc.defineParameter("experiment_3_flags", "Experiment 3 Flags (Default: \"\"", portal.ParameterType.STRING, "")
-pc.defineParameter("experiment_4_flags", "Experiment 4 Flags (Default: \"\"", portal.ParameterType.STRING, "")
-pc.defineParameter("experiment_5_flags", "Experiment 5 Flags (Default: \"\"", portal.ParameterType.STRING, "")
+pc.defineParameter("common_args",
+                   "Common Experiment Args (Default: \"ssb hash-aggregate\", replace with \"skip\" if not in use.)",
+                   portal.ParameterType.STRING, "ssb hash-aggregate")
+pc.defineParameter("experiment_1_args", "Experiment 1 Args (Default: \"skip\")", portal.ParameterType.STRING, "skip")
+pc.defineParameter("experiment_2_args", "Experiment 2 Args (Default: \"skip\")", portal.ParameterType.STRING, "skip")
+pc.defineParameter("experiment_3_args", "Experiment 3 Args (Default: \"skip\")", portal.ParameterType.STRING, "skip")
+pc.defineParameter("experiment_4_args", "Experiment 4 Args (Default: \"skip\")", portal.ParameterType.STRING, "skip")
+pc.defineParameter("experiment_5_args", "Experiment 5 Args (Default: \"skip\")", portal.ParameterType.STRING, "skip")
 
 params = portal.context.bindParameters()
 
@@ -70,18 +78,23 @@ out_params = {
     "hardware": params.hardware,
     "storage": params.storage,
     "scale_factor": params.scale_factor,
-    "experiment_1_flags": params.experiment_1_flags,
-    "experiment_2_flags": params.experiment_2_flags,
-    "experiment_3_flags": params.experiment_3_flags,
-    "experiment_4_flags": params.experiment_4_flags,
-    "experiment_5_flags": params.experiment_5_flags,
+    "common_args": params.common_args,
+    "experiment_1_args": params.experiment_1_args,
+    "experiment_2_args": params.experiment_2_args,
+    "experiment_3_args": params.experiment_3_args,
+    "experiment_4_args": params.experiment_4_args,
+    "experiment_5_args": params.experiment_5_args,
 }
+enc_str = url_parser.quote_plus((json.dumps(out_params, separators=(',', ':'))))
 
 execute_str = \
-    "sudo chmod +755 /local/repository/scripts/cloudlab/cloudlab_setup.sh;" + \
+    "sudo touch /mydata/params.json;" + \
+    "sudo chmod +777 /mydata/params.json;" + \
+    "echo " + enc_str + " > /mydata/params.json;" + \
+    "sudo chmod +777 /local/repository/scripts/cloudlab/cloudlab_setup.sh;" + \
     "/local/repository/scripts/cloudlab/cloudlab_setup.sh " + str(params.scale_factor) + ";" + \
-    "sudo chmod +755 /mydata/repo/scripts/cloudlab/cloudlab.py" + \
-    "/mydata/repo/scripts/cloudlab/cloudlab.py " + json.dumps(out_params) + " >> /mydata/report.txt;"
+    "sudo chmod +777 /mydata/repo/scripts/cloudlab/cloudlab.py;" + \
+    "python3 /mydata/repo/scripts/cloudlab/cloudlab.py >> /mydata/report.txt 2>&1;"
 node.addService(pg.Execute(shell="bash", command=execute_str))
 
 rspec.addResource(node)
