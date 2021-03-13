530d4b136279cec0e9fc3de8de2691c1ba73f501
Ginda Chen <32371474+GindaChen@users.noreply.github.com>
2021-03-13 00:13:22 -0600

    Update install_arrow.sh

--- install_arrow.sh
+++ install_arrow.sh
@@ -3,18 +3,25 @@
 # Checkout the arrow 3.0.0 Version
 if [ ! -d "arrow" ]; then
   git clone https://github.com/apache/arrow.git
-  cd arrow
+  cd arrow || exit 1
   git checkout apache-arrow-3.0.0
   cd ..
 fi
 
 # Make the release
 mkdir -p arrow/cpp/release
-cd arrow/cpp/release
+cd arrow/cpp/release || exit 1
+
+CMAKE_BIN=cmake
+CMAKE_FLAGS="-DARROW_COMPUTE=ON -DARROW_DEPENDENCY_SOURCE=AUTO"
+
 if [[ $(uname) == "Darwin" ]]; then
   # -DARROW_DEPENDENCY_SOURCE=AUTO: try to build dependencies from source if they are not found on the machine.
-  cmake -DARROW_COMPUTE=ON -DARROW_DEPENDENCY_SOURCE=AUTO ..
+  CMAKE_BIN=cmake
 elif [[ $(uname) == "Linux" ]]; then
-  ../../../cmake-3.15.5/bin/cmake -DARROW_COMPUTE=ON -DARROW_DEPENDENCY_SOURCE=AUTO ..
+  CMAKE_BIN=../../../cmake-3.15.5/bin/cmake
 fi
+
+# Build arrow
+${CMAKE_BIN} ${CMAKE_FLAGS} ..
 sudo make install -j4
