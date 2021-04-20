bc0ebbd6846d26ab465b3e1e8c00302e760b6187
Ginda Chen <32371474+GindaChen@users.noreply.github.com>
2021-04-20 11:48:39 -0500

    Update memlog_test.cc
    Try see which test can be unmuted

--- src/storage/tests/memlog_test.cc
+++ src/storage/tests/memlog_test.cc
@@ -1,38 +1,38 @@
-//// Licensed to the Apache Software Foundation (ASF) under one
-//// or more contributor license agreements.  See the NOTICE file
-//// distributed with this work for additional information
-//// regarding copyright ownership.  The ASF licenses this file
-//// to you under the Apache License, Version 2.0 (the
-//// "License"); you may not use this file except in compliance
-//// with the License.  You may obtain a copy of the License at
-////
-////   http://www.apache.org/licenses/LICENSE-2.0
-////
-//// Unless required by applicable law or agreed to in writing,
-//// software distributed under the License is distributed on an
-//// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-//// KIND, either express or implied.  See the License for the
-//// specific language governing permissions and limitations
-//// under the License.
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
 //
-//#include <filesystem>
-//#include <iostream>
+//   http://www.apache.org/licenses/LICENSE-2.0
 //
-//#include "gtest/gtest.h"
-//#include "storage/cmemlog.h"
-//
-//using namespace testing;
-//
-//class HustleMemLogTest : public testing::Test {
-// protected:
-//  void SetUp() override {
-//    hustle_memlog_initialize(&hustle_memlog, "test", MEMLOG_INIT_SIZE);
-//  }
-//
-//  HustleMemLog* hustle_memlog;
-//};
-//
-//TEST_F(HustleMemLogTest, MemlogTestForInsertion) {
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include <filesystem>
+#include <iostream>
+
+#include "gtest/gtest.h"
+#include "storage/cmemlog.h"
+
+using namespace testing;
+
+class HustleMemLogTest : public testing::Test {
+ protected:
+  void SetUp() override {
+    hustle_memlog_initialize(&hustle_memlog, "test", MEMLOG_INIT_SIZE);
+  }
+
+  HustleMemLog* hustle_memlog;
+};
+
+TEST_F(HustleMemLogTest, MemlogTestForInsertion) {
 //  DBRecord* record =
 //      hustle_memlog_create_record(MEMLOG_HUSTLE_INSERT, 0, "test", 4);
 //  int status = hustle_memlog_insert_record(hustle_memlog, record, 0);
@@ -50,9 +50,9 @@
 //  EXPECT_EQ(hustle_memlog->record_list[0].curr_size, 2);
 //  status = hustle_memlog_free(hustle_memlog);
 //  EXPECT_EQ(status, MEMLOG_OK);
-//}
+}
 //
-//TEST_F(HustleMemLogTest, MemlogTestForMultipleInsertion) {
+TEST_F(HustleMemLogTest, MemlogTestForMultipleInsertion) {
 //  DBRecord* record =
 //      hustle_memlog_create_record(MEMLOG_HUSTLE_INSERT, 0, "test", 4);
 //  int status = hustle_memlog_insert_record(hustle_memlog, record, 0);
@@ -84,7 +84,7 @@
 //  EXPECT_EQ(hustle_memlog->record_list[1].curr_size, 2);
 //  status = hustle_memlog_free(hustle_memlog);
 //  EXPECT_EQ(status, MEMLOG_OK);
-//}
+}
 //
 //TEST_F(HustleMemLogTest, MemlogTestForClear) {
 //  DBRecord* record =
