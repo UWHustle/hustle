abb631db56ac4a6bd52f08d77be5a288b534498c
Ginda Chen <32371474+GindaChen@users.noreply.github.com>
2021-04-21 16:32:12 -0500

    Update more headers

--- src/storage/tests/memlog_test.cc
+++ src/storage/tests/memlog_test.cc
@@ -21,8 +21,15 @@
 #include "gtest/gtest.h"
 #include "storage/cmemlog.h"
 #include "storage/base_table.h"
+#include "storage/hustle_table.h"
 #include "storage/indexed_block.h"
 #include "storage/index_aware_table.h"
+#include "storage/hustle_storable.h"
+#include "storage/base_table.h"
+#include "storage/indexed_block.h"
+#include "storage/index_aware_table.h"
+
+
 using namespace testing;
 
 class HustleMemLogTest : public testing::Test {
